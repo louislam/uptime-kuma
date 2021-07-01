ef028794ac3a551499c3d6008060e5d32e435019
LouisLam <louislam@users.noreply.github.com>
2021-07-01 17:05:02 +0800

    sort dashboard home event list

--- src/pages/DashboardHome.vue
+++ src/pages/DashboardHome.vue
@@ -117,6 +117,16 @@ export default {
                 }
             }
 
+            result.sort((a, b) => {
+                if (a.time > b.time) {
+                    return -1;
+                } else if (a.time < b.time) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            });
+
             return result;
         }
     }
