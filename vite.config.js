dffe3cf8f2e28803ef0f59a95ff158a6397448a1
LouisLam <louislam@users.noreply.github.com>
2021-08-05 18:20:34 +0800

    Revert "try to support subdirectory reverse proxy"
    This reverts commit a03dd91e40dc485ff95f635858760162f80f4372.

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
-import { defineConfig } from "vite"
-import vue from "@vitejs/plugin-vue"
-import legacy from "@vitejs/plugin-legacy"
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "./",
-    plugins: [
-        vue(),
-        legacy({
-            targets: ["ie > 11"],
-            additionalLegacyPolyfills: ["regenerator-runtime/runtime"]
-        })
-    ]
+  plugins: [
+      vue(),
+      legacy({
+          targets: ['ie > 11'],
+          additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+      })
+  ]
 })
