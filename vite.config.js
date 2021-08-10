413bd32aee0936237ca714cc61240d394dc80513
Louis Lam <louislam@users.noreply.github.com>
2021-08-10 15:54:28 +0800

    add bad code

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig({ 
   plugins: [
       vue(),
       legacy({
