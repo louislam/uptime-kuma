7ca80fc086b94e656d3027b42473d25898c8eb3a
Louis Lam <louislam@users.noreply.github.com>
2021-08-12 22:17:20 +0800

    fix auto theme

--- src/mixins/theme.js
+++ src/mixins/theme.js
@@ -2,7 +2,7 @@ export default {
 
     data() {
         return {
-            system: (window.matchMedia("(prefers-color-scheme: dark)")) ? "dark" : "light",
+            system: (window.matchMedia("(prefers-color-scheme: dark)").matches) ? "dark" : "light",
             userTheme: localStorage.theme,
         };
     },
