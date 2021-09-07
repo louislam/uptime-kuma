da74391c3e6c315d2f317f7f34224a50a4431520
LouisLam <louislam@users.noreply.github.com>
2021-09-07 22:42:46 +0800

    convert notifications into modules

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,8 @@ module.exports = {
     },
     rules: {
         "camelcase": ["warn", {
-            "properties": "never"
+            "properties": "never",
+            "ignoreImports": true
         }],
         // override/add rules settings here, such as:
         // 'vue/no-unused-vars': 'error'
