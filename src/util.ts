bcc02c1680ab032ce2602b2e3a2ae0dd9c97d1b3
Adam Stachowicz <adam.stachowicz@fingo.info>
2021-09-19 23:09:52 +0200

    Add formatting and comments from `util.ts` to `util.js`

--- src/util.ts
+++ src/util.ts
@@ -50,16 +50,15 @@ export function debug(msg: any) {
 
 declare global { interface String { replaceAll(str: string, newStr: string): string; } }
 
+/**
+ * String.prototype.replaceAll() polyfill
+ * https://gomakethings.com/how-to-replace-a-section-of-a-string-with-another-one-with-vanilla-js/
+ * @author Chris Ferdinandi
+ * @license MIT
+ */
 export function polyfill() {
-    /**
-     * String.prototype.replaceAll() polyfill
-     * https://gomakethings.com/how-to-replace-a-section-of-a-string-with-another-one-with-vanilla-js/
-     * @author Chris Ferdinandi
-     * @license MIT
-     */
     if (!String.prototype.replaceAll) {
         String.prototype.replaceAll = function (str: string, newStr: string) {
-
             // If a regex pattern
             if (Object.prototype.toString.call(str).toLowerCase() === "[object regexp]") {
                 return this.replace(str, newStr);
@@ -67,7 +66,6 @@ export function polyfill() {
 
             // If a string
             return this.replace(new RegExp(str, "g"), newStr);
-
         };
     }
 }
