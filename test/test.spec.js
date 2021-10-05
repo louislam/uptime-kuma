d25603e62909b586b325312b11f575b93dfc7a4b
LouisLam <louislam@users.noreply.github.com>
2021-10-05 16:03:35 +0800

    update jest test

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,19 +1,36 @@
-beforeAll(() => {
+const fs = require("fs");
 
+beforeAll(() => {
+    fs.rmdirSync("./data/test", {
+        recursive: true,
+    });
 });
 
 afterAll(() => {
-    return console.log("Cleanup");
+
 });
 
 describe("Very Simple Test", () => {
-
     const title = "Uptime Kuma";
 
     beforeAll(async () => {
         await page.goto("http://127.0.0.1:3002");
     });
 
+    it(`should be titled "${title}"`, async () => {
+        await expect(page.title()).resolves.toMatch(title);
+    });
+
+    it("Create an admin account", async () => {
+        await page.evaluate(() => document.);
+    });
+});
+
+describe("Status Page", () => {
+    const title = "Uptime Kuma";
+    beforeAll(async () => {
+        await page.goto("http://127.0.0.1:3002/status");
+    });
     it(`should be titled "${title}"`, async () => {
         await expect(page.title()).resolves.toMatch(title);
     });
