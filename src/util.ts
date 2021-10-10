11bcd1e2ed06651b8cf07e8df76ef20e5a6bca6a
Andreas Brett <andreasbrett@users.noreply.github.com>
2021-10-10 22:55:32 +0200

    const

--- src/util.ts
+++ src/util.ts
@@ -117,7 +117,7 @@ export function getRandomInt(min: number, max: number) {
 export function getCryptoRandomInt(min: number, max: number) {
     const randomBuffer = new Uint32Array(1);
     crypto.getRandomValues(randomBuffer);
-    let randomNumber = randomBuffer[0] / (0xffffffff + 1);
+    const randomNumber = randomBuffer[0] / (0xffffffff + 1);
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(randomNumber * (max - min + 1)) + min;
