3fe91c52cb994d5e8e0617d7a05854c1e8c6c193
zsxeee <zsxeee@qq.com>
2021-10-12 16:29:18 +0800

    Fix i18n
    Missing webhook json description
    Ajust Telegram context-based sentence, (also changed translated language files)
    Missing primary base url label
    Wrong PromoSMS i18n
    Missing Octopush legacy hint
    Missing Matrix i18n
    Missing push url i18n

--- src/components/notifications/Matrix.vue
+++ src/components/notifications/Matrix.vue
@@ -1,25 +1,25 @@
 <template>
     <div class="mb-3">
-        <label for="homeserver-url" class="form-label">Homeserver URL (with http(s):// and optionally port)</label><span style="color: red;"><sup>*</sup></span>
+        <label for="homeserver-url" class="form-label">{{ $t("matrixHomeserverURL") }}</label><span style="color: red;"><sup>*</sup></span>
         <input id="homeserver-url" v-model="$parent.notification.homeserverUrl" type="text" class="form-control" :required="true">
     </div>
     <div class="mb-3">
-        <label for="internal-room-id" class="form-label">Internal Room Id</label><span style="color: red;"><sup>*</sup></span>
+        <label for="internal-room-id" class="form-label">{{ $t("Internal Room Id") }}</label><span style="color: red;"><sup>*</sup></span>
         <input id="internal-room-id" v-model="$parent.notification.internalRoomId" type="text" class="form-control" required="true">
     </div>
     <div class="mb-3">
-        <label for="access-token" class="form-label">Access Token</label><span style="color: red;"><sup>*</sup></span>
+        <label for="access-token" class="form-label">{{ $t("Access Token") }}</label><span style="color: red;"><sup>*</sup></span>
         <HiddenInput id="access-token" v-model="$parent.notification.accessToken" :required="true" autocomplete="one-time-code" :maxlength="500"></HiddenInput>
     </div>
 
     <div class="form-text">
-        <span style="color: red;"><sup>*</sup></span>Required
+        <span style="color: red;"><sup>*</sup></span>{{ $t("Required") }}
         <p style="margin-top: 8px;">
-            You can find the internal room ID by looking in the advanced section of the room settings in your Matrix client. It should look like !QMdRCpUIfLwsfjxye6:home.server.
+            {{ $t("matrixDesc1") }}
         </p>
-        <p style="margin-top: 8px;">
-            It is highly recommended you create a new user and do not use your own Matrix user's access token as it will allow full access to your account and all the rooms you joined. Instead, create a new user and only invite it to the room that you want to receive the notification in. You can get the access token by running <code>curl -XPOST -d '{"type": "m.login.password", "identifier": {"user": "botusername", "type": "m.id.user"}, "password": "passwordforuser"}' "https://home.server/_matrix/client/r0/login"</code>.
-        </p>
+        <i18n-t tag="p" keypath="matrixDesc2" style="margin-top: 8px;">
+            <code>curl -XPOST -d '{"type": "m.login.password", "identifier": {"user": "botusername", "type": "m.id.user"}, "password": "passwordforuser"}' "https://home.server/_matrix/client/r0/login"</code>.
+        </i18n-t>
     </div>
 </template>
 
@@ -30,5 +30,5 @@ export default {
     components: {
         HiddenInput,
     },
-}
+};
 </script>
--- src/pages/EditMonitor.vue
+++ src/pages/EditMonitor.vue
@@ -49,8 +49,8 @@
                                 <label for="push-url" class="form-label">{{ $t("Push URL") }}</label>
                                 <CopyableInput id="push-url" v-model="pushURL" type="url" disabled="disabled" />
                                 <div class="form-text">
-                                    You should call this url every {{ monitor.interval }} seconds.<br />
-                                    Optional parameters: msg, ping
+                                    {{ $t("needPushEvery", [monitor.interval]) }}<br />
+                                    {{ $t("pushOptionalParams") }}
                                 </div>
                             </div>
 
