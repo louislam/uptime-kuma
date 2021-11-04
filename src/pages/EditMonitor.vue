0481a241f3905dc23421a41de6b0ca80836f233c
Ivan Bratović <ivanbratovic4@gmail.com>
2021-11-04 10:22:42 +0100

    Add translated placeholders for editing basic auth

--- src/pages/EditMonitor.vue
+++ src/pages/EditMonitor.vue
@@ -270,9 +270,9 @@
                                 <h4 class="mt-5 mb-2">{{ $t("HTTP Basic Auth") }}</h4>
                                 <div class="my-3">
                                     <label for="basicauth" class="form-label">{{ $t("Username") }}</label>
-                                    <input id="basicauth-user" v-model="monitor.basic_auth_user" type="text" class="form-control" :placeholder="Username">
+                                    <input id="basicauth-user" v-model="monitor.basic_auth_user" type="text" class="form-control" :placeholder="$t('Username')">
                                     <label for="basicauth" class="form-label">{{ $t("Password") }}</label>
-                                    <input id="basicauth-pass" v-model="monitor.basic_auth_pass" type="password" class="form-control" :placeholder="Password">
+                                    <input id="basicauth-pass" v-model="monitor.basic_auth_pass" type="password" class="form-control" :placeholder="$t('Password')">
                                 </div>
                             </template>
                         </div>
