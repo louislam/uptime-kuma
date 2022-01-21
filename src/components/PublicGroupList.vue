3dda5938f2180c4af2b9edd6f165c196c707da1c
Raphael Bernhart <mail@raphaelbernhart.at>
2022-01-21 15:39:49 +0100

    💄 Add condition to div tag for styling reasons

--- src/components/PublicGroupList.vue
+++ src/components/PublicGroupList.vue
@@ -41,7 +41,7 @@
                                             <Uptime :monitor="monitor.element" type="24" :pill="true" />
                                             {{ monitor.element.name }}
                                         </div>
-                                        <div class="tags">
+                                        <div v-if="monitor.element.tags.length" class="tags">
                                             <Tag v-for="tag in monitor.element.tags" :key="tag" :item="tag" :size="'sm'" />
                                         </div>
                                     </div>
