136fdf376860e3d2fd5ce8d48d9a22d8aeb62c2a
Louis Lam <louislam@users.noreply.github.com>
2022-04-17 01:07:36 +0800

    MQTT password field to password type

--- src/pages/EditMonitor.vue
+++ src/pages/EditMonitor.vue
@@ -122,17 +122,17 @@
                             <!-- For MQTT Type -->
                             <template v-if="monitor.type === 'mqtt'">
                                 <div class="my-3">
-                                    <label for="mqttUsername" class="form-label">{{ $t("Username") }}</label>
+                                    <label for="mqttUsername" class="form-label">MQTT {{ $t("Username") }}</label>
                                     <input id="mqttUsername" v-model="monitor.mqttUsername" type="text" class="form-control">
                                 </div>
 
                                 <div class="my-3">
-                                    <label for="mqttPassword" class="form-label">{{ $t("Password") }}</label>
-                                    <input id="mqttPassword" v-model="monitor.mqttPassword" type="text" class="form-control">
+                                    <label for="mqttPassword" class="form-label">MQTT {{ $t("Password") }}</label>
+                                    <input id="mqttPassword" v-model="monitor.mqttPassword" type="password" class="form-control">
                                 </div>
 
                                 <div class="my-3">
-                                    <label for="mqttTopic" class="form-label">{{ $t("Topic") }}</label>
+                                    <label for="mqttTopic" class="form-label">MQTT {{ $t("Topic") }}</label>
                                     <input id="mqttTopic" v-model="monitor.mqttTopic" type="text" class="form-control" required>
                                     <div class="form-text">
                                         {{ $t("topicExplanation") }}
@@ -140,7 +140,7 @@
                                 </div>
 
                                 <div class="my-3">
-                                    <label for="mqttSuccessMessage" class="form-label">{{ $t("successMessage") }}</label>
+                                    <label for="mqttSuccessMessage" class="form-label">MQTT {{ $t("successMessage") }}</label>
                                     <input id="mqttSuccessMessage" v-model="monitor.mqttSuccessMessage" type="text" class="form-control" required>
                                     <div class="form-text">
                                         {{ $t("successMessageExplanation") }}
