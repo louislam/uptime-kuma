bc174c33257009cae257ccd303135ca2ebb54fbf
Clemens Wolff <clemens@justamouse.com>
2022-05-02 11:00:14 -0400

    Extract child process args into variable

--- server/notification-providers/apprise.js
+++ server/notification-providers/apprise.js
@@ -6,7 +6,8 @@ class Apprise extends NotificationProvider {
     name = "apprise";
 
     async send(notification, msg, monitorJSON = null, heartbeatJSON = null) {
-        let s = childProcess.spawnSync("apprise", [ "-vv", "-b", msg, notification.appriseURL ]);
+        let args = [ "-vv", "-b", msg, notification.appriseURL ];
+        let s = childProcess.spawnSync("apprise", args);
 
         let output = (s.stdout) ? s.stdout.toString() : "ERROR: maybe apprise not found";
 
