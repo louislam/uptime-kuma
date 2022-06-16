986c03aecdedef034ec4fd11735118ec25b09482
tamasmagyar <20069588+tamasmagyar@users.noreply.github.com>
2022-06-16 11:28:17 +0200

    test cypress run

--- server/util-server.js
+++ server/util-server.js
@@ -508,6 +508,25 @@ exports.startUnitTest = async () => {
     });
 };
 
+exports.startE2eTests = async () => {
+    console.log("Starting unit test...");
+    const npm = /^win/.test(process.platform) ? "npm.cmd" : "npm";
+    const child = childProcess.spawn(npm, [ "run", "cy:run" ]);
+
+    child.stdout.on("data", (data) => {
+        console.log(data.toString());
+    });
+
+    child.stderr.on("data", (data) => {
+        console.log(data.toString());
+    });
+
+    child.on("close", function (code) {
+        console.log("Jest exit code: " + code);
+        process.exit(code);
+    });
+};
+
 /**
  * Convert unknown string to UTF8
  * @param {Uint8Array} body Buffer
