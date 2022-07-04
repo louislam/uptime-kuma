c4125a8334d0fb4c7737a96d928f745fa9360b0a
theS1LV3R <s1lv3r@corax.team>
2022-07-04 20:38:44 +0200

    style: fix linter error

--- server/server.js
+++ server/server.js
@@ -1677,7 +1677,7 @@ async function shutdownFunction(signal) {
 }
 
 function getClientIp(socket) {
-    return socket.client.conn.request.headers["x-forwarded-for"] 
+    return socket.client.conn.request.headers["x-forwarded-for"]
             || socket.client.conn.request.headers["x-real-ip"]
             || socket.client.conn.remoteAddress.replace(/^.*:/, "");
 }
