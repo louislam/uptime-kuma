648004e7deebe375d31063d8bfdc97590d6a23ae
zolotarev <andrey.zolotarev@umbrellait.com>
2022-08-04 16:10:56 +0300

    Fix inline styles according stylelint

--- src/pages/DashboardHome.vue
+++ src/pages/DashboardHome.vue
@@ -7,11 +7,11 @@
 
             <div class="shadow-box big-padding text-center mb-4">
                 <div class="row">
-                    <div class="col" :class="{'filter-selected':$root.statusFilter===1}" style="cursor: pointer" @click="$root.statusFilter=$root.statusFilter===1?undefined:1">
+                    <div class="col" :class="{'filter-selected':$root.statusFilter===1}" style="cursor: pointer;" @click="$root.statusFilter=$root.statusFilter===1?undefined:1">
                         <h3>{{ $t("Up") }}</h3>
                         <span class="num">{{ $root.stats.up }}</span>
                     </div>
-                    <div class="col" :class="{'filter-selected':$root.statusFilter===0}" style="cursor: pointer" @click="$root.statusFilter=$root.statusFilter===0?undefined:0">
+                    <div class="col" :class="{'filter-selected':$root.statusFilter===0}" style="cursor: pointer;" @click="$root.statusFilter=$root.statusFilter===0?undefined:0">
                         <h3>{{ $t("Down") }}</h3>
                         <span class="num text-danger">{{ $root.stats.down }}</span>
                     </div>
