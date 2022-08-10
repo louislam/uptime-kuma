3428b42140ec0c13a0e69847224a4a8e752c5f19
Andrew Zolotarev <zolotarev@its.bz>
2022-08-11 00:20:07 +0300

    Update src/mixins/socket.js
    Co-authored-by: Adam Stachowicz <saibamenppl@gmail.com>

--- src/mixins/socket.js
+++ src/mixins/socket.js
@@ -53,7 +53,7 @@ export default {
                 errorMessage: "",
                 currentPassword: "",
             },
-            statusFilter: undefined
+            statusFilter: undefined,
         };
     },
 
