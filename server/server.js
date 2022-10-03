3e699f8ac3c544cd0dd7d9459ef9b8da3c7efd65
George Tsomlektsis <ultrawelfaredev@gmail.com>
2022-10-03 18:01:52 +0300

    Fix linting errors.

--- server/server.js
+++ server/server.js
@@ -178,14 +178,14 @@ let needSetup = false;
 
         log.debug("entry", `Request Domain: ${hostname}`);
 
-        const uptimeKumaEntryPage=UptimeKumaServer.getInstance().entryPage;
+        const uptimeKumaEntryPage = UptimeKumaServer.getInstance().entryPage;
         if (hostname in StatusPage.domainMappingList) {
             log.debug("entry", "This is a status page domain");
 
             let slug = StatusPage.domainMappingList[hostname];
             await StatusPage.handleStatusPageResponse(response, server.indexHTML, slug);
 
-        } else if (uptimeKumaEntryPage && uptimeKumaEntryPage.startsWith('statusPage-')) {
+        } else if (uptimeKumaEntryPage && uptimeKumaEntryPage.startsWith("statusPage-")) {
             response.redirect("/status/" + uptimeKumaEntryPage.replace("statusPage-", ""));
 
         } else {
