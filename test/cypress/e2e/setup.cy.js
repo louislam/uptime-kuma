c5430e8defb4ab9401cfac04fc92b1e3b627213c
Louis Lam <louislam@users.noreply.github.com>
2022-10-04 20:56:45 +0800

    Fix typo and translate setup.cy.js

--- test/cypress/e2e/setup.cy.js
+++ test/cypress/e2e/setup.cy.js
@@ -1,22 +1,16 @@
-import { actor } from "../support/actors/actor";
-import { DEFAULT_USER_DATA } from "../support/const/user-data";
-import { DashboardPage } from "../support/pages/dasboard-page";
-import { SetupPage } from "../support/pages/setup-page";
+const actor = require("../support/actors/actor");
+const userData = require("../support/const/user-data");
+const dashboardPage = require("../support/pages/dashboard-page");
+const setupPage = require("../support/pages/setup-page");
 
 describe("user can create a new account on setup page", () => {
     before(() => {
         cy.visit("/setup");
     });
-
     it("user can create new account", () => {
-        cy.url().should("be.equal", SetupPage.url);
-        actor.setupTask.fillAndSubmitSetupForm(
-            DEFAULT_USER_DATA.username,
-            DEFAULT_USER_DATA.password,
-            DEFAULT_USER_DATA.password
-        );
-
-        cy.url().should("be.equal", DashboardPage.url);
+        cy.url().should("be.equal", setupPage.SetupPage.url);
+        actor.actor.setupTask.fillAndSubmitSetupForm(userData.DEFAULT_USER_DATA.username, userData.DEFAULT_USER_DATA.password, userData.DEFAULT_USER_DATA.password);
+        cy.url().should("be.equal", dashboardPage.DashboardPage.url);
         cy.get('[role="alert"]')
             .should("be.visible")
             .and("contain.text", "Added Successfully.");
