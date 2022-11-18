56a591899584fe9898f8ee6393d21f59067292ad
yangs1202 <me@yangs.kr>
2022-11-18 20:12:12 +0900

    feat/#2331:When monitoring HTTP, add a User Agent.

--- src/pages/EditMonitor.vue
+++ src/pages/EditMonitor.vue
@@ -292,15 +292,7 @@
                                 </label>
                                 <input id="resend-interval" v-model="monitor.resendInterval" type="number" class="form-control" required min="0" step="1">
                             </div>
-
-                            <div v-if="monitor.type === 'http'" class="my-3">
-                                <label for="userAgent" class="form-label">{{ $t("userAgent") }}</label>
-                                <input id="userAgent" v-model="monitor.userAgent" type="text" class="form-control" min="0" max="30" step="1">
-                                <div class="form-text">
-                                    {{ $t("userAgentDescription") }}
-                                </div>
-                            </div>
-
+                            
                             <h2 v-if="monitor.type !== 'push'" class="mt-5 mb-2">{{ $t("Advanced") }}</h2>
 
                             <div v-if="monitor.type === 'http' || monitor.type === 'keyword' " class="my-3 form-check">
@@ -466,6 +458,15 @@
                                     <label for="headers" class="form-label">{{ $t("Headers") }}</label>
                                     <textarea id="headers" v-model="monitor.headers" class="form-control" :placeholder="headersPlaceholder"></textarea>
                                 </div>
+
+                                <div class="my-3">
+                                    <label for="userAgent" class="form-label">{{ $t("userAgent") }}</label>
+                                    <input id="userAgent" v-model="monitor.userAgent" type="text" class="form-control" min="0" max="30" step="1">
+                                    <div class="form-text">
+                                        {{ $t("userAgentDescription") }}
+                                    </div>
+                                </div>
+
 
                                 <!-- HTTP Auth -->
                                 <h4 class="mt-5 mb-2">{{ $t("Authentication") }}</h4>
