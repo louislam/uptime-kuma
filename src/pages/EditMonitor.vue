15c64d458b69489b455ee90b13dd610446a091ec
Faruk Genç <omer@farukgenc.com>
2023-01-14 16:48:12 +0300

    Fix lint

--- src/pages/EditMonitor.vue
+++ src/pages/EditMonitor.vue
@@ -463,13 +463,13 @@
                                 </div>
 
                               <!-- Encoding -->
-                              <div class="my-3">
-                                <label for="httpBodyEncoding" class="form-label">{{ $t("Body Encoding") }}</label>
-                                <select id="httpBodyEncoding" v-model="monitor.httpBodyEncoding" class="form-select">
-                                  <option value="json">JSON</option>
-                                  <option value="xml">XML</option>
-                                </select>
-                              </div>
+                                <div class="my-3">
+                                    <label for="httpBodyEncoding" class="form-label">{{ $t("Body Encoding") }}</label>
+                                    <select id="httpBodyEncoding" v-model="monitor.httpBodyEncoding" class="form-select">
+                                      <option value="json">JSON</option>
+                                      <option value="xml">XML</option>
+                                    </select>
+                                </div>
 
                                 <!-- Body -->
                                 <div class="my-3">
