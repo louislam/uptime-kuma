aef85078eb5fbb0e48db31cd40f5d7eaf6c06792
Faruk Genç <omer@farukgenc.com>
2023-01-20 12:29:56 +0300

    reorder fix

--- server/model/monitor.js
+++ server/model/monitor.js
@@ -290,9 +290,9 @@ class Monitor extends BeanModel {
                         headers: {
                             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
                             "User-Agent": "Uptime-Kuma/" + version,
-                            ...(this.headers ? JSON.parse(this.headers) : {}),
+                            ...(contentType ? { "Content-Type": contentType } : {}),
                             ...(basicAuthHeader),
-                            ...(contentType ? { "Content-Type": contentType } : {})
+                            ...(this.headers ? JSON.parse(this.headers) : {})
                         },
                         maxRedirects: this.maxredirects,
                         validateStatus: (status) => {
