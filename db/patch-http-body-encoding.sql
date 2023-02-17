c9b4a7f53ef502a5ea58faf970102b6098d0b479
Faruk Genç <omer@farukgenc.com>
2023-02-17 17:59:43 +0300

    Change column type

--- db/patch-http-body-encoding.sql
+++ db/patch-http-body-encoding.sql
@@ -1,6 +1,6 @@
 -- You should not modify if this have pushed to Github, unless it does serious wrong with the db.
 BEGIN TRANSACTION;
 
-ALTER TABLE [monitor] ADD http_body_encoding TEXT;
+ALTER TABLE [monitor] ADD http_body_encoding VARCHAR(25);
 
 COMMIT;
