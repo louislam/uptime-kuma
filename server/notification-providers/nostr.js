d070a7810fe2692bee016b2ac78c8d8409fd13ba
John Danks <john.danks@gmail.com>
2023-04-06 08:00:52 -0700

    Add nostr DM notification provider

---
+++ server/notification-providers/nostr.js
@@ -0,0 +1,107 @@
+const NotificationProvider = require("./notification-provider");
+
+const {
+    relayInit,
+    getPublicKey,
+    getEventHash,
+    signEvent,
+    nip04,
+    nip19
+} = require("nostr-tools");
+require("websocket-polyfill");
+
+class Nostr extends NotificationProvider {
+
+    name = "nostr";
+
+    async send(notification, msg, monitorJSON = null, heartbeatJSON = null) {
+
+        // All DMs should have same timestamp
+        const createdAt = Math.floor(Date.now() / 1000);
+
+        const senderPrivateKey = await this.getPrivateKey(notification.sender);
+        const senderPublicKey = getPublicKey(senderPrivateKey);
+        const recipientsPublicKeys = await this.getPublicKeys(notification.recipients);
+
+        if (heartbeatJSON == null) {
+            msg = `🤙💜 Test Notification for [${notification.name}]`;
+        }
+
+        // Create NIP-04 encrypted direct message event for each recipient
+        const events = [];
+        for (const recipientPublicKey of recipientsPublicKeys) {
+            const ciphertext = await nip04.encrypt(senderPrivateKey, recipientPublicKey, msg);
+            let event = {
+                kind: 4,
+                pubkey: senderPublicKey,
+                created_at: createdAt,
+                tags: [[ "p", recipientPublicKey ]],
+                content: ciphertext,
+            };
+            event.id = getEventHash(event);
+            event.sig = signEvent(event, senderPrivateKey);
+            events.push(event);
+        }
+
+        // Publish events to each relay
+        const relays = notification.relays.split("\n");
+        const relayCount = relays.length;
+        let successfulRelays = 0;
+
+        // Connect to each relay
+        for (const relayUrl of relays) {
+            const relay = relayInit(relayUrl);
+            try {
+                await relay.connect();
+                successfulRelays++;
+
+                // Publish events
+                for (const event of events) {
+                    relay.publish(event);
+                }
+            } catch (error) {
+                continue;
+            } finally {
+                relay.close();
+            }
+        }
+
+        // Report success or failure
+        if (successfulRelays > 0) {
+            return `${successfulRelays}/${relayCount} relays connected.`;
+        } else {
+            throw Error("Failed to connect to any relays.");
+        }
+    }
+
+    async getPrivateKey(sender) {
+        try {
+            const senderDecodeResult = await nip19.decode(sender);
+            const { data } = senderDecodeResult;
+            return data;
+        } catch (error) {
+            throw new Error(`Failed to get private key: ${error.message}`);
+        }
+    }
+
+    async getPublicKeys(recipients) {
+        const recipientsList = recipients.split("\n");
+        const publicKeys = [];
+        for (const recipient of recipientsList) {
+            try {
+                const recipientDecodeResult = await nip19.decode(recipient);
+                const { type, data } = recipientDecodeResult;
+                if (type == "npub") {
+                    publicKeys.push(data);
+                } else {
+                    throw new Error("not an npub");
+                }
+            } catch (error) {
+                throw new Error(`Error decoding recipient: ${error}`);
+            }
+        }
+        return publicKeys;
+    }
+}
+
+module.exports = Nostr;
--- src/components/notifications/index.js
+++ src/components/notifications/index.js
@@ -18,6 +18,7 @@ import LineNotify from "./LineNotify.vue";
 import LunaSea from "./LunaSea.vue";
 import Matrix from "./Matrix.vue";
 import Mattermost from "./Mattermost.vue";
+import Nostr from "./Nostr.vue";
 import Ntfy from "./Ntfy.vue";
 import Octopush from "./Octopush.vue";
 import OneBot from "./OneBot.vue";
@@ -75,6 +76,7 @@ const NotificationFormList = {
     "lunasea": LunaSea,
     "matrix": Matrix,
     "mattermost": Mattermost,
+    "nostr": Nostr,
     "ntfy": Ntfy,
     "octopush": Octopush,
     "OneBot": OneBot,
