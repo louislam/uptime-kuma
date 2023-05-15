966066b897235579dfbac2d7ce639ea236aa63cc
Yoswaris Lawpaiboon <22832362+kiznick@users.noreply.github.com>
2023-05-15 13:56:59 +0700

    Update src/components/BadgeGeneratorDialog.vue
    Co-authored-by: Frank Elsinga <frank@elsinga.de>

--- src/components/BadgeGeneratorDialog.vue
+++ src/components/BadgeGeneratorDialog.vue
@@ -3,7 +3,7 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 id="exampleModalLabel" class="modal-title">
+                    <h5 class="modal-title">
                         {{ $t("Badge Generator", [monitor.name]) }}
                     </h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" />
