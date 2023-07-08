1a25e4502f8fb40dc15a428b94ef1431b2842406
SGpro <Mark520fay35@gmail.com>
2023-07-08 23:31:51 +0800

    Refactor TailscalePing & better error handling
    Split check function into two methods and added async/await syntax for readability/modularity
    Switched to promise-based error handling (takes into account different types of error such as "Execution error", "Error in output", "no matching peer", and "is local Tailscale IP") and throws them as JavaScript errors.

--- server/monitor-types/tailscale-ping.js
+++ server/monitor-types/tailscale-ping.js
@@ -14,39 +14,66 @@ const exec = require('child_process').exec;
 // 3 ping a nonexistent IP or inaccessible node
 // CMD: tailscale ping 100.100.100.3
 // no matching peer
+// 4 ping a peer that can only receive incoming connections
+// CMD: tailscale ping 100.100.100.4
+// multiple 'pong from' lines (pong from hostname (100.100.100.4) via DERP(tok) in 123ms) and a 'direct connection not established' line
+// We only care about the first 'pong from' line for the purpose of checking uptime.
 
 class TailscalePing extends MonitorType {
 
     name = "tailscale-ping";
 
     async check(monitor, heartbeat) {
-        // no let self???? no preserve context (this) for async function...
-        // should console.log() be implemented?
+        try {
+            let tailscaleOutput = await this.runTailscalePing(monitor.url);
+            this.parseTailscaleOutput(tailscaleOutput, heartbeat);
+        } catch (err) {
+            // trigger log function somewhere to display a notification or alert to the user (but how?)
+            throw new Error(`Error checking Tailscale ping: ${err}`);
+        }
+    }
 
-        let cmd = `tailscale ping ${monitor.url}`;
-       
-        // Should we use promise-based code? tp wrap child_process.exec() in a new promise?
-        childProcess.exec(cmd, (error, stdout, stderr) => {
-            if (error) {
-                throw new Error(`Execution error: ${error.message}`);
-            }
-            if (stderr) {
-                throw new Error(`Error in output: ${stderr}`);
-            }
+// runTailscalePing is now marked as async
+    async runTailscalePing(url) {
+        let cmd = `tailscale ping ${url}`;
+        return new Promise((resolve, reject) => {
+            //set a timeout here just incase tailscale ping hangs
+            exec(cmd, {timeout: 10000}, (error, stdout, stderr) => {
+                // Also to trigger a function here of error.message or stderr in the web gui 
+                // we may need to handle more cases if tailscale reports an error that isn't necessarily an error (such as not-logged in or DERP health-related issues) 
+                if (error) {
+                    reject(`Execution error: ${error.message}`);
+                    return;
+                }
+                if (stderr) {
+                    reject(`Error in output: ${stderr}`);
+                    return;
+                }
+                resolve(stdout);
+            });
+        });
+    }
+
+// logic for handling stdout is now moved to parseTailscaleOutput
+    parseTailscaleOutput(tailscaleOutput, heartbeat) {
+        let lines = tailscaleOutput.split('\n');
+
+        for (let line of lines) {
             // "pong from" counts as UP, and we update heartbeat status and ping time
             // otherwise if "no matching peer" (nonexistant or inaccessible due to ACLs) or "is local Tailscale IP" (ping self), throw error
             // otherwise throw error and the unexpected output itself
-            if (stdout.includes("pong from")) {
+            if (line.includes("pong from")) {
                 heartbeat.status = UP;
-                let time = stdout.split(" in ")[1].split(" ")[0];
+                let time = line.split(" in ")[1].split(" ")[0];
                 heartbeat.ping = parseInt(time);
-                heartbeat.msg = stdout;
-            } else if (stdout.includes("no matching peer") || stdout.includes("is local Tailscale IP")) {
-                throw new Error(stdout);
-            } else {
-                throw new Error(`Unexpected output: ${stdout}`);
+                heartbeat.msg = line;
+                break;
+            } else if (line.includes("no matching peer") || line.includes("is local Tailscale IP")) {
+                throw new Error(`"${line}"`);
+            } else if (line !== '') {
+                throw new Error(`Unexpected output: "${line}"`);
             }
-        });
+        }
     }
 }
 
