8c47727a33eb49c67c5f6f0bd554e039d1ae3d2f
SGpro <Mark520fay35@gmail.com>
2023-07-13 13:04:01 +0800

    eslint tailscale-ping.js --fix

--- server/monitor-types/tailscale-ping.js
+++ server/monitor-types/tailscale-ping.js
@@ -1,6 +1,6 @@
-const { MonitorType } = require("./monitor-type");
-const { UP } = require("../../src/util");
-const exec = require('child_process').exec;
+const { MonitorType } = require("./monitor-type")
+const { UP } = require("../../src/util")
+const exec = require("child_process").exec
 
 /**
  * A TailscalePing class extends the MonitorType.
@@ -8,78 +8,78 @@ const exec = require('child_process').exec;
  */
 class TailscalePing extends MonitorType {
 
-    name = "tailscale-ping";
-    
+    name = "tailscale-ping"
+
     /**
      * Checks the ping status of the URL associated with the monitor.
      * It then parses the Tailscale ping command output to update the heatrbeat.
-     * 
+     *
      * @param {Object} monitor - The monitor object associated with the check.
      * @param {Object} heartbeat - The heartbeat object to update.
      * @throws Will throw an error if checking Tailscale ping encounters any error
      */
     async check(monitor, heartbeat) {
         try {
-            let tailscaleOutput = await this.runTailscalePing(monitor.url);
-            this.parseTailscaleOutput(tailscaleOutput, heartbeat);
+            let tailscaleOutput = await this.runTailscalePing(monitor.url)
+            this.parseTailscaleOutput(tailscaleOutput, heartbeat)
         } catch (err) {
             // trigger log function somewhere to display a notification or alert to the user (but how?)
-            throw new Error(`Error checking Tailscale ping: ${err}`);
+            throw new Error(`Error checking Tailscale ping: ${err}`)
         }
     }
 
     /**
      * Runs the Tailscale ping command to the given URL.
-     * 
+     *
      * @param {string} url - The URL to ping.
      * @returns {Promise<string>} - A Promise that resolves to the output of the Tailscale ping command
      * @throws Will throw an error if the command execution encounters any error.
      */
     async runTailscalePing(url) {
-        let cmd = `tailscale ping ${url}`;
+        let cmd = `tailscale ping ${url}`
         return new Promise((resolve, reject) => {
-            let timeout = this.interval * 1000 * 0.8;
-            exec(cmd, {timeout: timeout}, (error, stdout, stderr) => {
-                // we may need to handle more cases if tailscale reports an error that isn't necessarily an error (such as not-logged in or DERP health-related issues) 
+            let timeout = this.interval * 1000 * 0.8
+            exec(cmd, { timeout: timeout }, (error, stdout, stderr) => {
+                // we may need to handle more cases if tailscale reports an error that isn't necessarily an error (such as not-logged in or DERP health-related issues)
                 if (error) {
-                    reject(`Execution error: ${error.message}`);
-                    return;
+                    reject(`Execution error: ${error.message}`)
+                    return
                 }
                 if (stderr) {
-                    reject(`Error in output: ${stderr}`);
-                    return;
+                    reject(`Error in output: ${stderr}`)
+                    return
                 }
-                resolve(stdout);
-            });
-        });
+                resolve(stdout)
+            })
+        })
     }
 
     /**
      * Parses the output of the Tailscale ping command to update the heartbeat.
-     * 
+     *
      * @param {string} tailscaleOutput - The output of the Tailscale ping command.
      * @param {Object} heartbeat - The heartbeat object to update.
      * @throws Will throw an eror if the output contains any unexpected string.
      */
     parseTailscaleOutput(tailscaleOutput, heartbeat) {
-        let lines = tailscaleOutput.split('\n');
+        let lines = tailscaleOutput.split("\n")
 
         for (let line of lines) {
             if (line.includes("pong from")) {
-                heartbeat.status = UP;
-                let time = line.split(" in ")[1].split(" ")[0];
-                heartbeat.ping = parseInt(time);
-                heartbeat.msg = line;
-                break;
+                heartbeat.status = UP
+                let time = line.split(" in ")[1].split(" ")[0]
+                heartbeat.ping = parseInt(time)
+                heartbeat.msg = line
+                break
             } else if (line.includes("timed out")) {
-                throw new Error(`Ping timed out: "${line}"`);
+                throw new Error(`Ping timed out: "${line}"`)
                 // Immediately throws upon "timed out" message, the server is expected to re-call the check function
             } else if (line.includes("no matching peer")) {
-                throw new Error(`Nonexistant or inaccessible due to ACLs: "${line}"`);
+                throw new Error(`Nonexistant or inaccessible due to ACLs: "${line}"`)
             } else if (line.includes("is local Tailscale IP")) {
-                throw new Error(`Tailscale only works if used on other machines: "${line}"`);
-            } else if (line !== '') {
-                throw new Error(`Unexpected output: "${line}"`);
+                throw new Error(`Tailscale only works if used on other machines: "${line}"`)
+            } else if (line !== "") {
+                throw new Error(`Unexpected output: "${line}"`)
             }
         }
     }
@@ -87,4 +87,4 @@ class TailscalePing extends MonitorType {
 
 module.exports = {
     TailscalePing,
-};
+}
