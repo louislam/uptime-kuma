b5c42e7be62ce885fd74f43701301cfce4adacd2
Frank Elsinga <frank@elsinga.de>
2023-07-24 23:57:43 +0200

    Apply suggestions from code review

--- src/pages/EditMonitor.vue
+++ src/pages/EditMonitor.vue
@@ -381,7 +381,7 @@
                                 </div>
                                 <div class="my-3">
                                     <label for="sqlQuery" class="form-label">{{ $t("Query") }}</label>
-                                    <textarea id="sqlQuery" v-model="monitor.databaseQuery" class="form-control" :placeholder="$t('sqlQueryExample: select getdate()')" required></textarea>
+                                    <textarea id="sqlQuery" v-model="monitor.databaseQuery" class="form-control" :placeholder="$t('Example:', [ 'select getdate()' ])" required></textarea>
                                 </div>
                             </template>
                             <!-- Redis -->
