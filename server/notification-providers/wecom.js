dee6d717a8c6f9c21041e09f8c79470b56e48651
KeJun <kejun1997@gmail.com>
2023-07-27 12:04:05 +0000

    feat: Better WeCom notification
    closed #3145

--- server/notification-providers/wecom.js
+++ server/notification-providers/wecom.js
@@ -1,52 +1,119 @@
 const NotificationProvider = require("./notification-provider");
 const axios = require("axios");
-const { DOWN, UP } = require("../../src/util");
+const { setting } = require("../util-server");
+const { DOWN, UP, getMonitorRelativeURL } = require("../../src/util");
 
 class WeCom extends NotificationProvider {
-
     name = "WeCom";
-
     async send(notification, msg, monitorJSON = null, heartbeatJSON = null) {
         let okMsg = "Sent Successfully.";
-
-        try {
-            let WeComUrl = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=" + notification.weComBotKey;
-            let config = {
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            };
-            let body = this.composeMessage(heartbeatJSON, msg);
-            await axios.post(WeComUrl, body, config);
-            return okMsg;
-        } catch (error) {
-            this.throwGeneralAxiosError(error);
-        }
-    }
-
-    /**
-     * Generate the message to send
-     * @param {Object} heartbeatJSON Heartbeat details (For Up/Down only)
-     * @param {string} msg General message
-     * @returns {Object}
-     */
-    composeMessage(heartbeatJSON, msg) {
-        let title;
-        if (msg != null && heartbeatJSON != null && heartbeatJSON["status"] === UP) {
-            title = "UptimeKuma Monitor Up";
-        }
-        if (msg != null && heartbeatJSON != null && heartbeatJSON["status"] === DOWN) {
-            title = "UptimeKuma Monitor Down";
-        }
-        if (msg != null) {
-            title = "UptimeKuma Message";
-        }
-        return {
-            msgtype: "text",
-            text: {
-                content: title + msg
-            }
+        let WeComUrl =
+      "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=" +
+      notification.weComBotKey;
+        let config = {
+            headers: {
+                "Content-Type": "application/json",
+            },
         };
+
+        // If heartbeatJSON is null, assume we're testing.
+        if (heartbeatJSON == null) {
+            await axios.post(
+                WeComUrl,
+                {
+                    msgtype: "text",
+                    text: {
+                        content: msg,
+                    },
+                },
+                config
+            );
+            return okMsg;
+        }
+
+        let address = "";
+        let clientUrl = "";
+
+        //#region computed address and clientUrl
+        switch (monitorJSON["type"]) {
+            case "ping":
+                address = monitorJSON["hostname"];
+                break;
+            case "port":
+            case "dns":
+            case "steam":
+                address = monitorJSON["hostname"];
+                if (monitorJSON["port"]) {
+                    address += ":" + monitorJSON["port"];
+                }
+                break;
+            default:
+                address = monitorJSON["url"];
+                break;
+        }
+        const baseURL = await setting("primaryBaseURL");
+        if (baseURL) {
+            clientUrl = `${baseURL}${getMonitorRelativeURL(monitorJSON["id"])}`;
+        }
+        // fallback address
+        if (!clientUrl) {
+            clientUrl = monitorJSON["type"] === "push" ? "https://github.com/louislam/uptime-kuma" : address;
+        }
+        //#endregion
+
+        const templateCard = {
+            card_type: "text_notice",
+            main_title: {
+                title: monitorJSON["name"],
+                desc: monitorJSON["type"] === "push" ? "Heartbeat" : address,
+            },
+            emphasis_content: {
+                title: "❓",
+                desc: "Your service is unknown.",
+            },
+            horizontal_content_list: [
+                {
+                    keyname: "Time",
+                    value: heartbeatJSON["time"],
+                },
+            ],
+            card_action: {
+                type: 1,
+                url: clientUrl,
+            },
+        };
+
+        if (heartbeatJSON["status"] === DOWN) {
+            templateCard.emphasis_content = {
+                title: "❌",
+                desc: "Your service went down.",
+            };
+            templateCard.horizontal_content_list.push({
+                keyname: "Error",
+                value: heartbeatJSON["msg"] == null ? "N/A" : heartbeatJSON["msg"],
+            });
+        }
+        if (heartbeatJSON["status"] === UP) {
+            templateCard.emphasis_content = {
+                title: "✅",
+                desc: "Your service is up!",
+            };
+            templateCard.horizontal_content_list.push({
+                keyname: "Ping",
+                value:
+          heartbeatJSON["ping"] == null ? "N/A" : heartbeatJSON["ping"] + " ms",
+            });
+        }
+
+        await axios.post(
+            WeComUrl,
+            {
+                msgtype: "template_card",
+                template_card: templateCard,
+            },
+            config
+        );
+        return okMsg;
     }
 }
 
