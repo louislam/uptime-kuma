c434bf375d6cb933c154b032b8ec54573cb1488e
Furkan İ <developer@furkanipek.com.tr>
2023-08-21 13:59:06 +0300

    Update server/notification-providers/bitrix24.js
    Co-authored-by: Frank Elsinga <frank@elsinga.de>

--- server/notification-providers/bitrix24.js
+++ server/notification-providers/bitrix24.js
@@ -2,7 +2,6 @@ const NotificationProvider = require("./notification-provider");
 const axios = require("axios");
 
 class Bitrix24 extends NotificationProvider {
-
     name = "Bitrix24";
 
     async send(notification, msg, monitorJSON = null, heartbeatJSON = null) {
