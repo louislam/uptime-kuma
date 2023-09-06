c1eea20bb52c9f9ac7a90a93b1ff23f757a76698
Frank Elsinga <frank@elsinga.de>
2023-09-06 14:31:39 +0200

    Fix: Improve dnsMessage handling (#3614)

--- server/monitor-types/dns.js
+++ server/monitor-types/dns.js
@@ -15,13 +15,13 @@ class DnsMonitorType extends MonitorType {
         let dnsRes = await dnsResolve(monitor.hostname, monitor.dns_resolve_server, monitor.port, monitor.dns_resolve_type);
         heartbeat.ping = dayjs().valueOf() - startTime;
 
-        if (monitor.dns_resolve_type === "A" || monitor.dns_resolve_type === "AAAA" || monitor.dns_resolve_type === "TXT") {
+        if (monitor.dns_resolve_type === "A" || monitor.dns_resolve_type === "AAAA" || monitor.dns_resolve_type === "TXT" || monitor.dns_resolve_type === "PTR") {
             dnsMessage += "Records: ";
             dnsMessage += dnsRes.join(" | ");
         } else if (monitor.dns_resolve_type === "CNAME" || monitor.dns_resolve_type === "PTR") {
-            dnsMessage = dnsRes[0];
+            dnsMessage += dnsRes[0];
         } else if (monitor.dns_resolve_type === "CAA") {
-            dnsMessage = dnsRes[0].issue;
+            dnsMessage += dnsRes[0].issue;
         } else if (monitor.dns_resolve_type === "MX") {
             dnsRes.forEach(record => {
                 dnsMessage += `Hostname: ${record.exchange} - Priority: ${record.priority} | `;
@@ -39,7 +39,7 @@ class DnsMonitorType extends MonitorType {
             dnsMessage = dnsMessage.slice(0, -2);
         }
 
-        if (monitor.dns_last_result !== dnsMessage) {
+        if (monitor.dns_last_result !== dnsMessage && dnsMessage !== undefined) {
             R.exec("UPDATE `monitor` SET dns_last_result = ? WHERE id = ? ", [ dnsMessage, monitor.id ]);
         }
 
