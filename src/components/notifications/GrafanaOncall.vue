df337fbda74f466fcc4b8394f4ec71ef8796a3fd
atmaniak <atmaniak@gmail.com>
2023-09-12 17:51:05 +0200

    Update src/components/notifications/GrafanaOncall.vue
    Co-authored-by: Frank Elsinga <frank@elsinga.de>

--- src/components/notifications/GrafanaOncall.vue
+++ src/components/notifications/GrafanaOncall.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="mb-3">
-        <label for="GrafanaOncallURL" class="form-label">{{ $t("Grafana Oncall URL") }}<span style="color: red;"><sup>*</sup></span></label>
+        <label for="GrafanaOncallURL" class="form-label">{{ $t("GrafanaOncallURL") }}<span style="color: red;"><sup>*</sup></span></label>
         <input id="GrafanaOncallURL" v-model="$parent.notification.GrafanaOncallURL" type="text" class="form-control" required>
     </div>
 </template>
