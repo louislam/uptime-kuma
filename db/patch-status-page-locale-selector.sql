c3b4ad85e5b55be2b23e584b75c031fefb5e29c2
Wampie Driessen <1059058+wampiedriessen@users.noreply.github.com>
2023-09-13 11:32:55 +0200

    Convert migration to knex migration

--- db/patch-status-page-locale-selector.sql
+++
@@ -1,5 +0,0 @@
--- You should not modify if this have pushed to Github, unless it does serious wrong with the db.
-BEGIN TRANSACTION;
-ALTER TABLE status_page ADD show_locale_selector BOOLEAN NOT NULL DEFAULT 0;
-ALTER TABLE status_page ADD default_locale TEXT DEFAULT "";
-COMMIT;
