9ef1e69ae0a62a03b5924ea2f534f0a9ea213845
Louis Lam <louislam@users.noreply.github.com>
2023-09-13 20:03:12 +0800

    Push Examples (#3735)

---
+++ extra/push-examples/powershell/index.ps1
@@ -0,0 +1,8 @@
+$pushURL = "https://example.com/api/push/key?status=up&msg=OK&ping="
+$interval = 60
+
+while ($true) {
+    $res = Invoke-WebRequest -Uri $pushURL
+    Write-Host "Pushed!"
+    Start-Sleep -Seconds $interval
+}
