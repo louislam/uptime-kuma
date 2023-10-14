2f9b787fa8eb509db245e20fcaf8fcce32fe5a0c
Louis Lam <louislam@users.noreply.github.com>
2023-10-14 21:06:23 +0800

    Drop backup

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -113,9 +113,6 @@ export default {
                 proxies: {
                     title: this.$t("Proxies"),
                 },
-                backup: {
-                    title: this.$t("Backup"),
-                },
                 about: {
                     title: this.$t("About"),
                 },
