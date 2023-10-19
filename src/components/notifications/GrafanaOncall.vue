9b599ccd1de600e64ade4485b3d03e288360c3b4
atmaniak <atmaniak@gmail.com>
2023-10-19 13:32:42 +0200

    Add Grafana Oncall notification provider (#2783)
    * Add Grafana Oncall notification provider
    
    * Fix linter errors
    
    * Remove useless variables
    
    * Remove test message
    
    Co-authored-by: Frank Elsinga <frank@elsinga.de>
    
    * spelling consistency
    
    * Update server/notification-providers/grafana-oncall.js
    
    Co-authored-by: Frank Elsinga <frank@elsinga.de>
    
    * Update server/notification-providers/grafana-oncall.js
    
    Co-authored-by: Frank Elsinga <frank@elsinga.de>
    
    * eslint requirements
    
    Co-authored-by: Matthew Nickson <mnickson@sidingsmedia.com>
    
    * Add Grafana Oncall translation
    
    * Update src/components/notifications/GrafanaOncall.vue
    
    Co-authored-by: Frank Elsinga <frank@elsinga.de>
    
    * Check empty url
    
    ---------
    
    Co-authored-by: Emmanuel Cohen <emmanuel.cohen@bso.co>
    Co-authored-by: Frank Elsinga <frank@elsinga.de>
    Co-authored-by: Matthew Nickson <mnickson@sidingsmedia.com>
    Co-authored-by: Louis Lam <louislam@users.noreply.github.com>

---
+++ src/components/notifications/GrafanaOncall.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="mb-3">
+        <label for="GrafanaOncallURL" class="form-label">{{ $t("GrafanaOncallURL") }}<span style="color: red;"><sup>*</sup></span></label>
+        <input id="GrafanaOncallURL" v-model="$parent.notification.GrafanaOncallURL" type="text" class="form-control" required>
+    </div>
+</template>
+
