775f37a8a10fe01327acb6b0a164a019e0a3177b
adammhancock@gmail.com <adammhancock@gmail.com>
2023-10-21 16:10:56 +0100

    Screenshot in modal

---
+++ src/components/ScreenshotDialog.vue
@@ -0,0 +1,49 @@
+<template>
+    <div ref="modal" class="modal fade" tabindex="-1">
+        <div class="modal-dialog modal-xl modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" />
+                </div>
+                <div class="modal-body"></div>
+                <img :src="imageURL">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Modal } from "bootstrap";
+
+export default {
+    props: {
+        imageURL: {
+            type: String,
+            required: true,
+        },
+    },
+    data() {
+        return {
+            modal: null,
+        };
+    },
+    mounted() {
+        this.modal = new Modal(this.$refs.modal);
+    },
+    methods: {
+        show() {
+            this.modal.show();
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/vars.scss";
+
+.dark {
+    .modal-dialog .form-text, .modal-dialog p {
+        color: $dark-font-color;
+    }
+}
+</style>
