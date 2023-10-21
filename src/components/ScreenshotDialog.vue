22addbef9c0e48041b98ecde1bec4946c65667ab
adammhancock@gmail.com <adammhancock@gmail.com>
2023-10-21 23:55:02 +0100

    Added title

--- src/components/ScreenshotDialog.vue
+++ src/components/ScreenshotDialog.vue
@@ -3,6 +3,9 @@
         <div class="modal-dialog modal-xl modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
+                    <h5 id="exampleModalLabel" class="modal-title">
+                        {{ $t("Browser Screenshot") }}
+                    </h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" />
                 </div>
                 <div class="modal-body"></div>
