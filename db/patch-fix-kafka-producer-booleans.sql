c0e2c0d0bb52fe94cfbe97e445e2c950385109d3
Muhammed Hussein Karimi <info@karimi.dev>
2023-10-27 15:14:29 +0330

    🐛 fix: boolean fields db patch table modify
    Signed-off-by: Muhammed Hussein Karimi <info@karimi.dev>

--- db/patch-fix-kafka-producer-booleans.sql
+++ db/patch-fix-kafka-producer-booleans.sql
@@ -2,9 +2,18 @@
 BEGIN TRANSACTION;
 
 ALTER TABLE monitor
-    MODIFY COLUMN kafka_producer_ssl BOOLEAN default 0 NOT NULL;
+    RENAME COLUMN kafka_producer_ssl TO kafka_producer_ssl_old;
 
 ALTER TABLE monitor
-    MODIFY COLUMN kafka_producer_allow_auto_topic_creation BOOLEAN default 0 NOT NULL;
+    RENAME COLUMN kafka_producer_allow_auto_topic_creation TO kafka_producer_allow_auto_topic_creation_old;
+
+ALTER TABLE monitor
+    ADD COLUMN kafka_producer_ssl BOOLEAN default 0 NOT NULL;
+
+ALTER TABLE monitor
+    ADD COLUMN kafka_producer_allow_auto_topic_creation BOOLEAN default 0 NOT NULL;
+
+UPDATE monitor set kafka_producer_allow_auto_topic_creation = monitor.kafka_producer_allow_auto_topic_creation_old;
+UPDATE monitor set kafka_producer_ssl = monitor.kafka_producer_ssl_old;
 
 COMMIT;
