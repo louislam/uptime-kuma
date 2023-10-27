adc9dfc2543e7c815fdf6a33a1167d1cbd8aad8f
Muhammed Hussein Karimi <info@karimi.dev>
2023-10-27 17:18:27 +0330

    ✏️  typo: remove `_old` COLUMNs in patch-fix-kafka-producer-booleans
    Signed-off-by: Muhammed Hussein Karimi <info@karimi.dev>

--- db/patch-fix-kafka-producer-booleans.sql
+++ db/patch-fix-kafka-producer-booleans.sql
@@ -1,19 +1,29 @@
 -- You should not modify if this have pushed to Github, unless it does serious wrong with the db.
 BEGIN TRANSACTION;
 
+-- Rename COLUMNs to another one (suffixed by `_old`)
 ALTER TABLE monitor
     RENAME COLUMN kafka_producer_ssl TO kafka_producer_ssl_old;
 
 ALTER TABLE monitor
     RENAME COLUMN kafka_producer_allow_auto_topic_creation TO kafka_producer_allow_auto_topic_creation_old;
 
+-- Add correct COLUMNs
 ALTER TABLE monitor
     ADD COLUMN kafka_producer_ssl BOOLEAN default 0 NOT NULL;
 
 ALTER TABLE monitor
     ADD COLUMN kafka_producer_allow_auto_topic_creation BOOLEAN default 0 NOT NULL;
 
+-- Set bring old values from `_old` COLUMNs to correct ones
 UPDATE monitor set kafka_producer_allow_auto_topic_creation = monitor.kafka_producer_allow_auto_topic_creation_old;
 UPDATE monitor set kafka_producer_ssl = monitor.kafka_producer_ssl_old;
+
+-- Remove old COLUMNs
+ALTER TABLE monitor
+    DROP COLUMN kafka_producer_allow_auto_topic_creation_old;
+
+ALTER TABLE monitor
+    DROP COLUMN kafka_producer_ssl_old;
 
 COMMIT;
