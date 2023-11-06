14290e93607e4808d25f0a80b00f7e985a9d4330
Muhammed Hussein Karimi <info@karimi.dev>
2023-11-06 15:37:23 +0330

    🐛 fix: kafka producer booleans migration null values
    Signed-off-by: Muhammed Hussein Karimi <info@karimi.dev>

--- db/patch-fix-kafka-producer-booleans.sql
+++ db/patch-fix-kafka-producer-booleans.sql
@@ -16,8 +16,11 @@ ALTER TABLE monitor
     ADD COLUMN kafka_producer_allow_auto_topic_creation BOOLEAN default 0 NOT NULL;
 
 -- Set bring old values from `_old` COLUMNs to correct ones
-UPDATE monitor set kafka_producer_allow_auto_topic_creation = monitor.kafka_producer_allow_auto_topic_creation_old;
-UPDATE monitor set kafka_producer_ssl = monitor.kafka_producer_ssl_old;
+UPDATE monitor SET kafka_producer_allow_auto_topic_creation = monitor.kafka_producer_allow_auto_topic_creation_old
+    WHERE monitor.kafka_producer_allow_auto_topic_creation_old IS NOT NULL;
+
+UPDATE monitor SET kafka_producer_ssl = monitor.kafka_producer_ssl_old
+    WHERE monitor.kafka_producer_ssl_old IS NOT NULL;
 
 -- Remove old COLUMNs
 ALTER TABLE monitor
