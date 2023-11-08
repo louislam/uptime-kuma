d73a17fd8ab65bf7a82f3badc8bbc1aee6216b34
Stephen Papierski <stephenpapierski@gmail.com>
2023-11-08 14:50:03 -0700

    Add threshold calculations methods

--- db/knex_migrations/2023-10-26-slow-response-notification.js
+++ db/knex_migrations/2023-10-26-slow-response-notification.js
@@ -3,9 +3,11 @@ exports.up = function (knex) {
     return knex.schema
         .alterTable("monitor", function(table) {
             table.boolean("slow_response_notification").notNullable().defaultTo(false);
-            table.integer("slow_response_notification_threshold").notNullable().defaultTo(0);
-            table.integer("slow_response_notification_range").notNullable().defaultTo(0);
             table.string("slow_response_notification_method").notNullable().defaultTo("");
+            table.integer("slow_response_notification_range").notNullable().defaultTo(0);
+            table.string("slow_response_notification_threshold_method").notNullable().defaultTo("");
+            table.integer("slow_response_notification_threshold").notNullable().defaultTo(0);
+            table.float("slow_response_notification_threshold_multiplier").notNullable().defaultTo(0.0);
             table.integer("slow_response_notification_resend_interval").notNullable().defaultTo(0);
         })
         .alterTable("heartbeat", function(table) {
@@ -14,15 +16,18 @@ exports.up = function (knex) {
 }
 
 exports.down = function (knex) {
+    // remove various slow_response_notification parameters
     return knex.schema
         .alterTable("monitor", function(table) {
-            table.boolean("slow_response_notification").notNullable().defaultTo(false);
-            table.integer("slow_response_notification_threshold").notNullable().defaultTo(0);
-            table.integer("slow_response_notification_range").notNullable().defaultTo(0);
-            table.string("slow_response_notification_method").notNullable().defaultTo("");
-            table.integer("slow_response_notification_resend_interval").notNullable().defaultTo(0);
+            table.dropColumn("slow_response_notification");
+            table.dropColumn("slow_response_notification_method");
+            table.dropColumn("slow_response_notification_range");
+            table.dropColumn("slow_response_notification_threshold_method");
+            table.dropColumn("slow_response_notification_threshold");
+            table.dropColumn("slow_response_notification_threshold_multiplier");
+            table.dropColumn("slow_response_notification_resend_interval");
         })
         .alterTable("heartbeat", function(table) {
-            table.integer("slow_response_count").notNullable().defaultTo(0);
+            table.dropColumn("slow_response_count");
         });
 }
