9964b6c4d88fb76948513366bc119583a42845cb
Nelson Chan <3271800+chakflying@users.noreply.github.com>
2023-11-16 20:41:35 +0800

    Fix: Update monitor object on pause (#4032)

--- server/server.js
+++ server/server.js
@@ -1832,6 +1832,7 @@ async function pauseMonitor(userID, monitorID) {
 
     if (monitorID in server.monitorList) {
         server.monitorList[monitorID].stop();
+        server.monitorList[monitorID].active = 0;
     }
 }
 
