20ccfaa6aa0ea053a2dc36c335826ae4bf6bbb60
Louis Lam <louislam@users.noreply.github.com>
2023-11-16 20:49:22 +0800

    Fix again

--- db/patch-fix-kafka-producer-booleans.sql
+++ db/patch-fix-kafka-producer-booleans.sql
@@ -15,12 +15,14 @@ ALTER TABLE monitor
 ALTER TABLE monitor
     ADD COLUMN kafka_producer_allow_auto_topic_creation BOOLEAN default 0 NOT NULL;
 
--- Set bring old values from `_old` COLUMNs to correct ones
-UPDATE monitor SET kafka_producer_allow_auto_topic_creation = monitor.kafka_producer_allow_auto_topic_creation_old
-    WHERE monitor.kafka_producer_allow_auto_topic_creation_old IS NOT NULL;
+-- These SQL is still not fully safe. See https://github.com/louislam/uptime-kuma/issues/4039.
 
-UPDATE monitor SET kafka_producer_ssl = monitor.kafka_producer_ssl_old
-    WHERE monitor.kafka_producer_ssl_old IS NOT NULL;
+-- Set bring old values from `_old` COLUMNs to correct ones
+-- UPDATE monitor SET kafka_producer_allow_auto_topic_creation = monitor.kafka_producer_allow_auto_topic_creation_old
+-- WHERE monitor.kafka_producer_allow_auto_topic_creation_old IS NOT NULL;
+
+-- UPDATE monitor SET kafka_producer_ssl = monitor.kafka_producer_ssl_old
+-- WHERE monitor.kafka_producer_ssl_old IS NOT NULL;
 
 -- Remove old COLUMNs
 ALTER TABLE monitor
