3500b049ef46559e02548c8c0d803431b76eb439
Frank Elsinga <frank@elsinga.de>
2023-11-30 18:14:04 +0100

    made the status domain selector more accessible

--- src/pages/StatusPage.vue
+++ src/pages/StatusPage.vue
@@ -69,13 +69,13 @@
                 <div class="my-3">
                     <label class="form-label">
                         {{ $t("Domain Names") }}
-                        <font-awesome-icon icon="plus-circle" class="btn-add-domain action text-primary" @click="addDomainField" />
+                        <font-awesome-icon role="button" icon="plus-circle" class="action text-primary" :aria-label="$t('Add a domain')" @click="addDomainField"/>
                     </label>
 
                     <ul class="list-group domain-name-list">
                         <li v-for="(domain, index) in config.domainNameList" :key="index" class="list-group-item">
                             <input v-model="config.domainNameList[index]" type="text" class="no-bg domain-input" placeholder="example.com" />
-                            <font-awesome-icon icon="times" class="action remove ms-2 me-3 text-danger" @click="removeDomain(index)" />
+                            <font-awesome-icon icon="times" class="action remove ms-2 me-3 text-danger" :aria-label="$t('Remove domain', domain)" role="button" @click="removeDomain(index)" />
                         </li>
                     </ul>
                 </div>
