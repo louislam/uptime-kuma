34da8e271ad3d0b7086c1302a4251e66e18e6902
Joschua Becker <jb@scolasti.co>
2023-12-12 04:55:35 +0000

    added: sevenio notification handler

---
+++ server/notification-providers/sevenio.js
@@ -0,0 +1,78 @@
+const NotificationProvider = require("./notification-provider");
+const Axios = require("axios");
+const { DOWN, UP } = require("../../src/util");
+
+class SevenIO extends NotificationProvider {
+
+    name = "SevenIO";
+
+    /**
+     * @inheritdoc
+     */
+    async send(notification, msg, monitorJSON = null, heartbeatJSON = null) {
+        const okMsg = "Sent Successfully.";
+
+        const data = {
+            to: notification.sevenioTo,
+            from: notification.sevenioSender || "Uptime Kuma",
+            text: msg,
+        };
+
+        const axios = Axios.create({
+            baseURL: "https://gateway.seven.io/api/",
+            headers: {
+                "Content-Type": "application/json",
+                "X-API-Key": notification.sevenioApiKey,
+            },
+        });
+
+        try {
+            // If heartbeatJSON is null, assume we're testing.
+            if (heartbeatJSON == null) {
+                await axios.post("sms", data);
+                return okMsg;
+            }
+
+            let address;
+
+            switch (monitorJSON["type"]) {
+                case "ping":
+                    address = monitorJSON["hostname"];
+                    break;
+                case "port":
+                case "dns":
+                case "gamedig":
+                case "steam":
+                    address = monitorJSON["hostname"];
+                    if (monitorJSON["port"]) {
+                        address += ":" + monitorJSON["port"];
+                    }
+                    break;
+                default:
+                    address = monitorJSON["url"];
+                    break;
+            }
+
+            // If heartbeatJSON is not null, we go into the normal alerting loop.
+            if (heartbeatJSON["status"] === DOWN) {
+                data.text = `Your service ${monitorJSON["name"]} (${address}) went down at ${heartbeatJSON["localDateTime"]} ` +
+                    `(${heartbeatJSON["timezone"]}). Error: ${heartbeatJSON["msg"]}`;
+
+                await axios.post("sms", data);
+                return okMsg;
+
+            } else if (heartbeatJSON["status"] === UP) {
+                data.text = `Your service ${monitorJSON["name"]} (${address}) went back up at ${heartbeatJSON["localDateTime"]} ` +
+                    `(${heartbeatJSON["timezone"]}).`;
+
+                await axios.post("sms", data);
+                return okMsg;
+            }
+        } catch (error) {
+            this.throwGeneralAxiosError(error);
+        }
+    }
+
+}
+
+module.exports = SevenIO;
---
+++ src/components/notifications/SevenIO.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="mb-3">
+        <label for="discord-webhook-url" class="form-label">{{ $t("apiKeySevenIO") }}</label>
+        <input id="discord-webhook-url" v-model="$parent.notification.sevenioApiKey" type="text" class="form-control" required autocomplete="false">
+        <div class="form-text">
+            {{ $t("wayToGetSevenIOApiKey") }}
+        </div>
+    </div>
+
+    <div class="mb-3">
+        <label for="discord-username" class="form-label">{{ $t("senderSevenIO") }}</label>
+        <input id="discord-username" v-model="$parent.notification.sevenioSender" type="text" class="form-control" autocomplete="false" placeholder="0123456789">
+    </div>
+
+    <div class="mb-3">
+        <label for="discord-prefix-message" class="form-label">{{ $t("receiverSevenIO") }}</label>
+        <input id="discord-prefix-message" v-model="$parent.notification.sevenioReceiver" type="text" class="form-control" autocomplete="false" placeholder="0123456789">
+        <div class="form-text">
+            {{ $t("receiverInfoSevenIO") }}
+        </div>
+    </div>
+</template>
