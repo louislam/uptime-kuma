7904db89854640a91377a44e92b55166cb4ba590
Joschua Becker <jb@scolasti.co>
2023-12-12 06:08:50 +0100

    fix: wrong naming for input field ids

--- src/components/notifications/SevenIO.vue
+++ src/components/notifications/SevenIO.vue
@@ -1,20 +1,20 @@
 <template>
     <div class="mb-3">
-        <label for="discord-webhook-url" class="form-label">{{ $t("apiKeySevenIO") }}</label>
-        <input id="discord-webhook-url" v-model="$parent.notification.sevenioApiKey" type="text" class="form-control" required autocomplete="false">
+        <label for="sevenio-api-key" class="form-label">{{ $t("apiKeySevenIO") }}</label>
+        <input id="sevenio-api-key" v-model="$parent.notification.sevenioApiKey" type="text" class="form-control" required autocomplete="false">
         <div class="form-text">
             {{ $t("wayToGetSevenIOApiKey") }}
         </div>
     </div>
 
     <div class="mb-3">
-        <label for="discord-username" class="form-label">{{ $t("senderSevenIO") }}</label>
-        <input id="discord-username" v-model="$parent.notification.sevenioSender" type="text" class="form-control" autocomplete="false" placeholder="0123456789">
+        <label for="sevenio-sender" class="form-label">{{ $t("senderSevenIO") }}</label>
+        <input id="sevenio-sender" v-model="$parent.notification.sevenioSender" type="text" class="form-control" autocomplete="false" placeholder="0123456789">
     </div>
 
     <div class="mb-3">
-        <label for="discord-prefix-message" class="form-label">{{ $t("receiverSevenIO") }}</label>
-        <input id="discord-prefix-message" v-model="$parent.notification.sevenioReceiver" type="text" class="form-control" autocomplete="false" placeholder="0123456789">
+        <label for="sevenio-receiver" class="form-label">{{ $t("receiverSevenIO") }}</label>
+        <input id="sevenio-receiver" v-model="$parent.notification.sevenioReceiver" type="text" class="form-control" autocomplete="false" placeholder="0123456789">
         <div class="form-text">
             {{ $t("receiverInfoSevenIO") }}
         </div>
