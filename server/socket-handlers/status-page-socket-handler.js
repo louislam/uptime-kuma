9ec2d2a58c23621829202aa4ac11594427680af0
laupse <lgrondin@ingeniance.fr>
2023-12-20 15:23:22 +0100

    Make auto refresh interval customizable
    Fix lint errore
    
    Remove manual french traduction
    
    Remove wrong comment
    
    Rollback wrong change for status api

--- server/socket-handlers/status-page-socket-handler.js
+++ server/socket-handlers/status-page-socket-handler.js
@@ -155,6 +155,7 @@ module.exports.statusPageSocketHandler = (socket) => {
             statusPage.title = config.title;
             statusPage.description = config.description;
             statusPage.icon = config.logo;
+            statusPage.autoRefreshInterval = config.autoRefreshInterval,
             statusPage.theme = config.theme;
             //statusPage.published = ;
             //statusPage.search_engine_index = ;
@@ -280,6 +281,7 @@ module.exports.statusPageSocketHandler = (socket) => {
             statusPage.title = title;
             statusPage.theme = "auto";
             statusPage.icon = "";
+            statusPage.autoRefreshInterval = 300;
             await R.store(statusPage);
 
             callback({
