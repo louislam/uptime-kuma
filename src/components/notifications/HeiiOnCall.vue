10659d07b261fc0542c5584d0429b7101fb8266e
Humberto Evans <hevans66@gmail.com>
2024-02-12 16:56:41 -0800

    add link to documentation explaining how to get id and token

--- src/components/notifications/HeiiOnCall.vue
+++ src/components/notifications/HeiiOnCall.vue
@@ -3,9 +3,7 @@
         <label for="heiioncall-apikey" class="form-label">{{ $t("API Key")
         }}<span style="color: red"><sup>*</sup></span></label>
         <HiddenInput
-            id="heiioncall-apikey"
-            v-model="$parent.notification.heiiOnCallApiKey"
-            required="true"
+            id="heiioncall-apikey" v-model="$parent.notification.heiiOnCallApiKey" required="true"
             autocomplete="false"
         ></HiddenInput>
     </div>
@@ -13,11 +11,14 @@
         <label for="heiioncall-trigger-id" class="form-label">{{ $t("Trigger ID")
         }}<span style="color: red"><sup>*</sup></span></label>
         <HiddenInput
-            id="heiioncall-trigger-id"
-            v-model="$parent.notification.heiiOnCallTriggerId"
-            required="true"
+            id="heiioncall-trigger-id" v-model="$parent.notification.heiiOnCallTriggerId" required="true"
             autocomplete="false"
         ></HiddenInput>
+    </div>
+    <div class="form-text">
+        <i18n-t tag="p" keypath="wayToGetHeiiOnCallDetails" style="margin-top: 8px;">
+            <a href="https://heiioncall.com/docs" target="_blank">Heii On-Call Documentation</a>
+        </i18n-t>
     </div>
 </template>
 
