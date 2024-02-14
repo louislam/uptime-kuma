cc000117ca9b8f888c69a31b9c8f2841b79bab7f
Frank Elsinga <frank@elsinga.de>
2024-02-14 23:41:35 +0100

    Updated the code to look more like the other notification providers

--- server/notification-providers/heii-oncall.js
+++ server/notification-providers/heii-oncall.js
@@ -10,6 +10,7 @@ class HeiiOnCall extends NotificationProvider {
      * @inheritdoc
      */
     async send(notification, msg, monitorJSON = null, heartbeatJSON = null) {
+        const okMsg = "Sent Successfully.";
         const payload = heartbeatJSON || {};
 
         const baseURL = await setting("primaryBaseURL");
@@ -17,34 +18,6 @@ class HeiiOnCall extends NotificationProvider {
             payload["url"] = baseURL + getMonitorRelativeURL(monitorJSON.id);
         }
 
-        try {
-            if (!heartbeatJSON) {
-                // Testing or general notification like certificate expiry
-                payload["msg"] = msg;
-                return this.postNotification(notification, "alert", payload);
-            }
-
-            if (heartbeatJSON.status === DOWN) {
-                return this.postNotification(notification, "alert", payload);
-            }
-
-            if (heartbeatJSON.status === UP) {
-                return this.postNotification(notification, "resolve", payload);
-            }
-        } catch (error) {
-            this.throwGeneralAxiosError(error);
-        }
-
-    }
-
-    /**
-     * Post to Heii On-Call
-     * @param {BeanModel} notification Message title
-     * @param {string} action Trigger action (alert, resovle)
-     * @param {object} payload Data for Heii On-Call
-     * @returns {Promise<string>} Success message
-     */
-    async postNotification(notification, action, payload) {
         const config = {
             headers: {
                 Accept: "application/json",
@@ -52,15 +25,29 @@ class HeiiOnCall extends NotificationProvider {
                 Authorization: "Bearer " + notification.heiiOnCallApiKey,
             },
         };
+        
+        const heiiUrl = `https://heiioncall.com/triggers/${notification.heiiOnCallTriggerId}/`;
+        
+        // docs https://heiioncall.com/docs#manual-triggers
+        try {
+            if (!heartbeatJSON) {
+                // Testing or general notification like certificate expiry
+                payload["msg"] = msg;
+                await axios.post(heiiUrl + "alert", payload, config);
+                return okMsg;
+            }
 
-        // Post to Heii On-Call Trigger https://heiioncall.com/docs#manual-triggers
-        await axios.post(
-            `https://heiioncall.com/triggers/${notification.heiiOnCallTriggerId}/${action}`,
-            payload,
-            config
-        );
-
-        return "Sent Successfully";
+            if (heartbeatJSON.status === DOWN) {
+                await axios.post(heiiUrl + "alert", payload, config);
+                return okMsg;
+            }
+            if (heartbeatJSON.status === UP) {
+                await axios.post(heiiUrl + "resolve",payload, config);
+                return okMsg;
+            }
+        } catch (error) {
+            this.throwGeneralAxiosError(error);
+        }
     }
 }
 
