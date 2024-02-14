aae0673a3dc30319fb14351cfb48c29d8df72b03
NihadBadalov <32594553+NihadBadalov@users.noreply.github.com>
2024-02-14 10:34:06 +0100

    Feat: Redirect /link to /dashboard for non-mobile users

--- src/pages/List.vue
+++ src/pages/List.vue
@@ -6,12 +6,18 @@
 
 <script>
 import MonitorList from "../components/MonitorList.vue";
+import mobile from "../mixins/mobile";
 
 export default {
     components: {
         MonitorList,
     },
 };
+
+mobile.methods.onResize();
+if (!mobile.computed.isMobile(mobile.methods.windowWidth) && window.location.pathname === "/list") {
+    window.location.href = "/dashboard";
+}
 </script>
 
 <style lang="scss" scoped>
