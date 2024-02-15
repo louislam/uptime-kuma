94154c49aabc22019ec814cea274226030ca6467
NihadBadalov <32594553+NihadBadalov@users.noreply.github.com>
2024-02-15 08:21:43 +0100

    Feat: Change hover colors to shades of gray

--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -212,7 +212,11 @@ export default {
 
 .nav-link {
     &:hover {
-        background-color: rgba($primary, 0.2);
+        background-color: rgba(0, 0, 0, 0.05);
+
+        .dark & {
+            background-color: rgba(255, 255, 255, 0.05);
+        }
     }
 
     &.status-page {
