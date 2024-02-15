7ce1cd71c577da92893215077bddfafb3770f634
Frank Elsinga <frank@elsinga.de>
2024-02-15 21:58:51 +0100

    improved the helptexts a bit

--- src/components/notifications/GtxMessaging.vue
+++ src/components/notifications/GtxMessaging.vue
@@ -7,18 +7,34 @@
         </div>
     </div>
     <div class="mb-3">
-        <label for="gtxmessaging-from" class="form-label">{{ $t("gtxMessagingFrom") }}</label>
+        <label for="gtxmessaging-from" class="form-label">{{ $t("From Phone Number / Transmission Path Originating Address (TPOA)") }}</label>
         <input id="gtxmessaging-from" v-model="$parent.notification.gtxMessagingFrom" type="text" class="form-control" required>
-        <div class="form-text">
-            {{ $t("gtxMessagingFromHint") }}
-        </div>
+        <i18n-t tag="div" keypath="gtxMessagingFromHint" class="form-text">
+            <template #e164>
+                <a href="https://wikipedia.org/wiki/E.164">E.164</a>
+            </template>
+            <template #e212>
+                <a href="https://wikipedia.org/wiki/E.212">E.212</a>
+            </template>
+            <template #e214>
+                <a href="https://wikipedia.org/wiki/E.214">E.214</a>
+            </template>
+        </i18n-t>
     </div>
     <div class="mb-3">
-        <label for="gtxmessaging-to" class="form-label">{{ $t("gtxMessagingTo") }}</label>
+        <label for="gtxmessaging-to" class="form-label">{{ $t("To Phone Number") }}</label>
         <input id="gtxmessaging-to" v-model="$parent.notification.gtxMessagingTo" type="text" pattern="^\+\d+$" class="form-control" required>
-        <div class="form-text">
-            {{ $t("gtxMessagingToHint") }}
-        </div>
+        <i18n-t tag="div" keypath="gtxMessagingToHint" class="form-text">
+            <template #e164>
+                <a href="https://wikipedia.org/wiki/E.164">E.164</a>
+            </template>
+            <template #e212>
+                <a href="https://wikipedia.org/wiki/E.212">E.212</a>
+            </template>
+            <template #e214>
+                <a href="https://wikipedia.org/wiki/E.214">E.214</a>
+            </template>
+        </i18n-t>
     </div>
 </template>
 
