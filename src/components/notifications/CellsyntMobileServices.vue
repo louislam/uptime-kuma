c34ec0dc9114a521eb7e07490a9ab86ec72edbb0
Simon Nilsson <mail@simonnilsson.com>
2024-03-24 13:38:41 +0100

    QA release

--- src/components/notifications/CellsyntMobileServices.vue
+++ src/components/notifications/CellsyntMobileServices.vue
@@ -10,23 +10,23 @@
     <div class="mb-3">
         <label for="cellsynt-Originatortype" class="form-label">{{ $t("Originator type") }}</label>
         <select id="cellsynt-Originatortype" v-model="$parent.notification.cellsyntOriginatortype" :required="true" class="form-select">
-            <option value="numeric" :selected="true">{{ $t("Numeric") }}</option>
+            <option value="numeric">{{ $t("Numeric") }}</option>
             <option value="shortcode">{{ $t("Shortcode") }}</option>
             <option value="alpha">{{ $t("Alpha") }}</option>
         </select>
         <div class="form-text">
-            <p><b>numeric:</b><br>
+            <p><b>Numeric:</b><br>
                 Numeric value (max 15 digits) with telephone number on
                 international format without leading 00 (example UK number
                 07920 110 000 should be set as 447920110000). Receiving
                 mobile phone will add a leading + sign and thus see the
                 originator as a normal mobile phone number (+447920110000).
                 Therefore it is also possible to reply to the message.</p>
-                <p><b>shortcode:</b><br>
+                <p><b>Shortcode:</b><br>
                 Numerical value (max 15 digits). Used to set a shortcode in an
                 operator network as originator (i.e. will be shown without leading
                 + sign, e.g. 72456).</p>
-                <p><b>alpha:</b><br>
+                <p><b>Alpha:</b><br>
                 Alphanumeric string (max 11 characters). The following
                 characters are guaranteed to work: a-z, A-Z and 0-9. Other
                 characters may work but functionality can not be guaranteed.
@@ -56,6 +56,12 @@ numbers with comma. Max 25000 recipients per HTTP request.</p></div>
         <div class="form-text">Long SMS (also known as "concatenated SMS") enables sending messages exceeding 160
 characters.</div>
     </div>
+    <div class="mb-3">
+        <label for="cellsynt-destination" class="form-label">{{ $t("Details") }}</label>
+        <div class="form-text">
+            For sign-up and price information check out this link, <a href="https://www.cellsynt.com/en/" target="_blank">https://www.cellsynt.com/en/</a>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -63,7 +69,7 @@ import HiddenInput from "../HiddenInput.vue";
 
 export default {
     components: {
-        HiddenInput,
-    },
+        HiddenInput
+    }
 };
 </script>
