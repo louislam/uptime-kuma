b82590f644de376609a8a107040234412ddde775
rasheed <thcmofskot@gmail.com>
2024-04-17 23:57:39 +0100

    Added restart policies and log file paths to enhance application reliability and monitoring

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,5 +2,8 @@ module.exports = {
     apps: [{
         name: "uptime-kuma",
         script: "./server/server.js",
+        restart_delay: 3000,
+        error_file: "./logs/uptime-kuma.err.log",
+        out_file: "./logs/uptime-kuma.out.log"
     }]
 };
