47d69921ec4a27ff8b852216234017790d4ffaf2
Frank Elsinga <frank@elsinga.de>
2024-04-19 01:55:58 +0000

    Tested how CI likes if the tests use `toEqual` instead of `equal`

--- test/cypress/unit/i18n.spec.js
+++ test/cypress/unit/i18n.spec.js
@@ -15,34 +15,34 @@ describe("Test i18n.js", () => {
         }
         setLanguage('en-EN');
 
-        expect(currentLocale()).equal("en");
+        expect(currentLocale()).toEqual("en");
 
         setLanguage('zh-HK');
-        expect(currentLocale()).equal("zh-HK");
+        expect(currentLocale()).toEqual("zh-HK");
 
         // Note that in Safari on iOS prior to 10.2, the country code returned is lowercase: "en-us", "fr-fr" etc.
         // https://developer.mozilla.org/en-US/docs/Web/API/Navigator/language
         setLanguage('zh-hk');
-        expect(currentLocale()).equal("en");
+        expect(currentLocale()).toEqual("en");
 
         setLanguage('en-US');
-        expect(currentLocale()).equal("en");
+        expect(currentLocale()).toEqual("en");
 
         setLanguage('ja-ZZ');
-        expect(currentLocale()).equal("ja");
+        expect(currentLocale()).toEqual("ja");
 
         setLanguage('zz-ZZ');
-        expect(currentLocale()).equal("en");
+        expect(currentLocale()).toEqual("en");
 
         setLanguage('zz-ZZ');
-        expect(currentLocale()).equal("en");
+        expect(currentLocale()).toEqual("en");
 
         setLanguage('en');
         localStorage.locale = "en";
-        expect(currentLocale()).equal("en");
+        expect(currentLocale()).toEqual("en");
 
         localStorage.locale = "zh-HK";
-        expect(currentLocale()).equal("zh-HK");
+        expect(currentLocale()).toEqual("zh-HK");
     });
 
 });
