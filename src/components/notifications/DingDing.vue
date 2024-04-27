4ce77e2c23427fe815856aac0379d9a17dc1dc6a
凯观生活 <niujinkai@vip.qq.com>
2024-04-27 02:34:03 +0000

    Add a configuration item for whether to notify everyone in DingTalk notification method

--- src/components/notifications/DingDing.vue
+++ src/components/notifications/DingDing.vue
@@ -12,5 +12,11 @@
                 <a href="https://developers.dingtalk.com/document/robots/custom-robot-access" target="_blank">https://developers.dingtalk.com/document/robots/custom-robot-access</a> <a href="https://open.dingtalk.com/document/robots/customize-robot-security-settings#title-7fs-kgs-36x" target="_blank">https://open.dingtalk.com/document/robots/customize-robot-security-settings#title-7fs-kgs-36x</a>
             </i18n-t>
         </div>
+
+        <label for="isAtAll" class="form-label">{{ $t("isAtAll") }}<span style="color: red;"><sup>*</sup></span></label>
+        <select id="isAtAll" v-model="$parent.notification.isAtAll" class="form-select" required>
+            <option value="true">{{ $t("TRUE") }}</option>
+            <option value="false">{{ $t("FALSE") }}</option>
+        </select>
     </div>
 </template>
