ec61acd4c23b8fac39b94ae042d6cab311dad674
Ezhil Shanmugham <ezhil930@gmail.com>
2024-04-28 23:23:15 +0530

    feat: keephq notification provider

--- src/components/notifications/Keep.vue
+++ src/components/notifications/Keep.vue
@@ -9,44 +9,34 @@
             class="form-control"
             required
         />
+        <div class="form-text">
+            <i18n-t tag="p" keypath="Read more:">
+                <a href="https://docs.keephq.dev/providers/documentation/uptimekuma-provider" target="_blank">https://docs.keephq.dev/providers/documentation/uptimekuma-provider</a>
+            </i18n-t>
+        </div>
     </div>
 
     <div class="mb-3">
-        <label for="webhook-apikey" class="form-label">{{ $t("API Key") }}</label>
-        <input
+        <label for="webhook-apikey" class="form-label">{{
+            $t("API Key")
+        }}</label>
+        <HiddenInput
             id="webhook-apikey"
             v-model="$parent.notification.webhookAPIKey"
-            type="text"
-            class="form-control"
-            required
-        />
+            :required="true"
+        ></HiddenInput>
     </div>
 </template>
 
 <script>
+import HiddenInput from "../HiddenInput.vue";
+
 export default {
-    computed: {
-        headersPlaceholder() {
-            return this.$t("Example:", [
-`{
-  "Authorization": "Authorization Token"
-}`,
-            ]);
-        },
-        customBodyPlaceholder() {
-            return this.$t("Example:", [
-`{
-  "Title": "Uptime Kuma Alert{% if monitorJSON %} - {{ monitorJSON['name'] }}{% endif %}",
-  "Body": "{{ msg }}"
-}`
-            ]);
-        }
+    components: {
+        HiddenInput,
+    },
+    mounted() {
+        this.$parent.notification.webhookURL ||= "";
     },
 };
 </script>
-
-<style lang="scss" scoped>
-textarea {
-    min-height: 200px;
-}
-</style>
