ff5890a11f0ef3e69959d04e5a43c7b808663787
Matt Visnovsky <matt@visnovsky.us>
2024-04-29 15:59:21 -0600

    Updated a comment

--- db/knex_migrations/2024-04-26-0000-snmp-monitor.js
+++ db/knex_migrations/2024-04-26-0000-snmp-monitor.js
@@ -1,7 +1,7 @@
 exports.up = function (knex) {
     return knex.schema
         .alterTable("monitor", function (table) {
-            table.string("snmp_community_string", 255).defaultTo("public"); // Add community_string column
+            table.string("snmp_community_string", 255).defaultTo("public"); // Add snmp_community_string column
             table.string("snmp_oid").notNullable(); // Add oid column
             table.enum("snmp_version", ["1", "2c", "3"]).defaultTo("2c"); // Add snmp_version column with enum values
             table.float("snmp_control_value").notNullable(); // Add control_value column as float
