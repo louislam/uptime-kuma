f11ae4137bc89d0c393ef5fa723a9285c0cf3c7c
Nicolas Verlhiac <nicolas@novariom.com>
2024-05-22 00:01:24 +0200

    SmsPartner : i18n + Pattern

--- src/components/notifications/SMSPartner.vue
+++ src/components/notifications/SMSPartner.vue
@@ -2,17 +2,24 @@
     <div class="mb-3">
         <label for="smspartner-key" class="form-label">{{ $t("API Key") }}</label>
         <HiddenInput id="smspartner-key" v-model="$parent.notification.smspartnerApikey" :required="true" autocomplete="new-password"></HiddenInput>
-        <i18n-t keypath="smspartnerApiurl" as="div" class="form-text">
-            <a href="https://my.smspartner.fr/dashboard/api" target="_blank">https://my.smspartner.fr/dashboard/api</a>
-        </i18n-t>
+        <div class="form-text">
+            <i18n-t keypath="smspartnerApiurl" as="div" class="form-text">
+                <a href="https://my.smspartner.fr/dashboard/api" target="_blank">https://my.smspartner.fr/dashboard/api</a>
+            </i18n-t>
+        </div>
     </div>
     <div class="mb-3">
         <label for="smspartner-phone-number" class="form-label">{{ $t("smspartnerPhoneNumber") }}</label>
         <input id="smspartner-phone-number" v-model="$parent.notification.smspartnerPhoneNumber" type="text" class="form-control" required>
+        <div class="form-text">
+            <i18n-t keypath="multiple numbers must be separated by" as="div" class="form-text">
+                <code>,</code>
+            </i18n-t>
+        </div>
     </div>
     <div class="mb-3">
         <label for="smspartner-sender-name" class="form-label">{{ $t("smspartnerSenderName") }}</label>
-        <input id="smspartner-sender-name" v-model="$parent.notification.smspartnerSenderName" type="text" minlength="3" maxlength="11" class="form-control">
+        <input id="smspartner-sender-name" v-model="$parent.notification.smspartnerSenderName" type="text" minlength="3" maxlength="11" pattern="^[a-zA-Z0-9]*$" class="form-control" required>
         <div class="form-text">
             {{ $t("smspartnerSenderNameInfo") }}
         </div>
