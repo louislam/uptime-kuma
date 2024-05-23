b2ede38269795f2181beb023736bdb3bf3ab8bb6
Stefan Ottosson <stefan@ottosson.io>
2024-05-23 17:42:36 +0200

    fix: update some examples

--- extra/push-examples/bash-curl/index.sh
+++ extra/push-examples/bash-curl/index.sh
@@ -2,9 +2,10 @@
 # Filename: index.sh
 PUSH_URL="https://example.com/api/push/key?status=up&msg=OK&ping="
 INTERVAL=60
+HTTP_METHOD="GET"
 
 while true; do
-    curl -s -o /dev/null $PUSH_URL
+    curl -X $HTTP_METHOD -s -o /dev/null $PUSH_URL
     echo "Pushed!"
     sleep $INTERVAL
 done
--- extra/push-examples/javascript-fetch/index.js
+++ extra/push-examples/javascript-fetch/index.js
@@ -1,9 +1,10 @@
 // Supports: Node.js >= 18, Deno, Bun
 const pushURL = "https://example.com/api/push/key?status=up&msg=OK&ping=";
 const interval = 60;
+const method = "GET";
 
 const push = async () => {
-    await fetch(pushURL);
+    await fetch(pushURL, { method });
     console.log("Pushed!");
 };
 
