2d0e7b3859c1a5a60413ddd55bca74e0a39ee661
Martin Rubli <martin@rubli.info>
2024-06-01 13:39:11 +0200

    tls: db: monitors: Rename request/start_tls fields to have a 'tcp_' prefix

--- db/knex_migrations/2024-04-30-0000-tls.js
+++ db/knex_migrations/2024-04-30-0000-tls.js
@@ -1,18 +1,18 @@
 exports.up = function (knex) {
-    // Add new column monitor.request
+    // Add new columns monitor.tcp_request and monitor.tcp_start_tls
     return knex.schema
         .alterTable("monitor", function (table) {
-            table.text("request").defaultTo(null);
-            table.boolean("start_tls").notNullable().defaultTo(false);
+            table.text("tcp_request").defaultTo(null);
+            table.boolean("tcp_start_tls").notNullable().defaultTo(false);
         });
 
 };
 
 exports.down = function (knex) {
-    // Drop column monitor.request
+    // Drop columns monitor.tcp_request and monitor.tcp_start_tls
     return knex.schema
         .alterTable("monitor", function (table) {
-            table.dropColumn("request");
-            table.dropColumn("start_tls");
+            table.dropColumn("tcp_request");
+            table.dropColumn("tcp_start_tls");
         });
 };
