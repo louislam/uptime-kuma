2ba7cf72bc42f24f923a4d76a43bdbe0a3957c0d
Martin Rubli <martin@rubli.info>
2024-06-02 20:13:21 +0200

    tls: db: monitors: Add fields for parametrizing STARTTLS prompt/command/response

--- db/knex_migrations/2024-04-30-0000-tls.js
+++ db/knex_migrations/2024-04-30-0000-tls.js
@@ -1,18 +1,24 @@
 exports.up = function (knex) {
-    // Add new columns monitor.tcp_request and monitor.tcp_start_tls
     return knex.schema
         .alterTable("monitor", function (table) {
             table.text("tcp_request").defaultTo(null);
+            table.boolean("tcp_enable_tls").notNullable().defaultTo(false);
             table.boolean("tcp_start_tls").notNullable().defaultTo(false);
+            table.string("tcp_start_tls_prompt", 63).defaultTo(null);
+            table.string("tcp_start_tls_command", 63).defaultTo(null);
+            table.string("tcp_start_tls_response", 63).defaultTo(null);
         });
 
 };
 
 exports.down = function (knex) {
-    // Drop columns monitor.tcp_request and monitor.tcp_start_tls
     return knex.schema
         .alterTable("monitor", function (table) {
             table.dropColumn("tcp_request");
+            table.dropColumn("tcp_enable_tls");
             table.dropColumn("tcp_start_tls");
+            table.dropColumn("tcp_start_tls_prompt");
+            table.dropColumn("tcp_start_tls_command");
+            table.dropColumn("tcp_start_tls_response");
         });
 };
