b8b876f603d72dd86b074ccc7bebc2736d50de0c
Louis Lam <louislam@users.noreply.github.com>
2024-06-24 00:02:36 +0800

    Revert eslint changes

--- server/modules/axios-ntlm/lib/hash.js
+++ server/modules/axios-ntlm/lib/hash.js
@@ -1,41 +1,29 @@
-"use strict";
+'use strict';
 // Original source at https://github.com/elasticio/node-ntlm-client/blob/master/lib/hash.js
-let crypto = require("crypto");
-/**
- * @param challenge
- * @param lmhash
- */
+var crypto = require('crypto');
 function createLMResponse(challenge, lmhash) {
-    let buf = new Buffer.alloc(24);
-    let pwBuffer = new Buffer.alloc(21).fill(0);
+    var buf = new Buffer.alloc(24), pwBuffer = new Buffer.alloc(21).fill(0);
     lmhash.copy(pwBuffer);
     calculateDES(pwBuffer.slice(0, 7), challenge).copy(buf);
     calculateDES(pwBuffer.slice(7, 14), challenge).copy(buf, 8);
     calculateDES(pwBuffer.slice(14), challenge).copy(buf, 16);
     return buf;
 }
-/**
- * @param password
- */
 function createLMHash(password) {
-    let buf = new Buffer.alloc(16); let pwBuffer = new Buffer.alloc(14); let magicKey = new Buffer.from("KGS!@#$%", "ascii");
+    var buf = new Buffer.alloc(16), pwBuffer = new Buffer.alloc(14), magicKey = new Buffer.from('KGS!@#$%', 'ascii');
     if (password.length > 14) {
         buf.fill(0);
         return buf;
     }
     pwBuffer.fill(0);
-    pwBuffer.write(password.toUpperCase(), 0, "ascii");
+    pwBuffer.write(password.toUpperCase(), 0, 'ascii');
     return Buffer.concat([
         calculateDES(pwBuffer.slice(0, 7), magicKey),
         calculateDES(pwBuffer.slice(7), magicKey)
     ]);
 }
-/**
- * @param key
- * @param message
- */
 function calculateDES(key, message) {
-    let desKey = new Buffer.alloc(8);
+    var desKey = new Buffer.alloc(8);
     desKey[0] = key[0] & 0xFE;
     desKey[1] = ((key[0] << 7) & 0xFF) | (key[1] >> 1);
     desKey[2] = ((key[1] << 6) & 0xFF) | (key[2] >> 2);
@@ -44,75 +32,48 @@ function calculateDES(key, message) {
     desKey[5] = ((key[4] << 3) & 0xFF) | (key[5] >> 5);
     desKey[6] = ((key[5] << 2) & 0xFF) | (key[6] >> 6);
     desKey[7] = (key[6] << 1) & 0xFF;
-    for (let i = 0; i < 8; i++) {
-        let parity = 0;
-        for (let j = 1; j < 8; j++) {
+    for (var i = 0; i < 8; i++) {
+        var parity = 0;
+        for (var j = 1; j < 8; j++) {
             parity += (desKey[i] >> j) % 2;
         }
         desKey[i] |= (parity % 2) === 0 ? 1 : 0;
     }
-    let des = crypto.createCipheriv("DES-ECB", desKey, "");
+    var des = crypto.createCipheriv('DES-ECB', desKey, '');
     return des.update(message);
 }
-/**
- * @param challenge
- * @param ntlmhash
- */
 function createNTLMResponse(challenge, ntlmhash) {
-    let buf = new Buffer.alloc(24);
-    let ntlmBuffer = new Buffer.alloc(21).fill(0);
+    var buf = new Buffer.alloc(24), ntlmBuffer = new Buffer.alloc(21).fill(0);
     ntlmhash.copy(ntlmBuffer);
     calculateDES(ntlmBuffer.slice(0, 7), challenge).copy(buf);
     calculateDES(ntlmBuffer.slice(7, 14), challenge).copy(buf, 8);
     calculateDES(ntlmBuffer.slice(14), challenge).copy(buf, 16);
     return buf;
 }
-/**
- * @param password
- */
 function createNTLMHash(password) {
-    let md4sum = crypto.createHash("md4");
-    md4sum.update(new Buffer.from(password, "ucs2"));
+    var md4sum = crypto.createHash('md4');
+    md4sum.update(new Buffer.from(password, 'ucs2'));
     return md4sum.digest();
 }
-/**
- * @param ntlmhash
- * @param username
- * @param authTargetName
- */
 function createNTLMv2Hash(ntlmhash, username, authTargetName) {
-    let hmac = crypto.createHmac("md5", ntlmhash);
-    hmac.update(new Buffer.from(username.toUpperCase() + authTargetName, "ucs2"));
+    var hmac = crypto.createHmac('md5', ntlmhash);
+    hmac.update(new Buffer.from(username.toUpperCase() + authTargetName, 'ucs2'));
     return hmac.digest();
 }
-/**
- * @param type2message
- * @param username
- * @param ntlmhash
- * @param nonce
- * @param targetName
- */
 function createLMv2Response(type2message, username, ntlmhash, nonce, targetName) {
-    let buf = new Buffer.alloc(24); let ntlm2hash = createNTLMv2Hash(ntlmhash, username, targetName); let hmac = crypto.createHmac("md5", ntlm2hash);
+    var buf = new Buffer.alloc(24), ntlm2hash = createNTLMv2Hash(ntlmhash, username, targetName), hmac = crypto.createHmac('md5', ntlm2hash);
     //server challenge
     type2message.challenge.copy(buf, 8);
     //client nonce
-    buf.write(nonce || createPseudoRandomValue(16), 16, "hex");
+    buf.write(nonce || createPseudoRandomValue(16), 16, 'hex');
     //create hash
     hmac.update(buf.slice(8));
-    let hashedBuffer = hmac.digest();
+    var hashedBuffer = hmac.digest();
     hashedBuffer.copy(buf);
     return buf;
 }
-/**
- * @param type2message
- * @param username
- * @param ntlmhash
- * @param nonce
- * @param targetName
- */
 function createNTLMv2Response(type2message, username, ntlmhash, nonce, targetName) {
-    let buf = new Buffer.alloc(48 + type2message.targetInfo.buffer.length); let ntlm2hash = createNTLMv2Hash(ntlmhash, username, targetName); let hmac = crypto.createHmac("md5", ntlm2hash);
+    var buf = new Buffer.alloc(48 + type2message.targetInfo.buffer.length), ntlm2hash = createNTLMv2Hash(ntlmhash, username, targetName), hmac = crypto.createHmac('md5', ntlm2hash);
     //the first 8 bytes are spare to store the hashed value before the blob
     //server challenge
     type2message.challenge.copy(buf, 8);
@@ -124,13 +85,13 @@ function createNTLMv2Response(type2message, username, ntlmhash, nonce, targetNam
     //TODO: we are loosing precision here since js is not able to handle those large integers
     // maybe think about a different solution here
     // 11644473600000 = diff between 1970 and 1601
-    let timestamp = ((Date.now() + 11644473600000) * 10000).toString(16);
-    let timestampLow = Number("0x" + timestamp.substring(Math.max(0, timestamp.length - 8)));
-    let timestampHigh = Number("0x" + timestamp.substring(0, Math.max(0, timestamp.length - 8)));
+    var timestamp = ((Date.now() + 11644473600000) * 10000).toString(16);
+    var timestampLow = Number('0x' + timestamp.substring(Math.max(0, timestamp.length - 8)));
+    var timestampHigh = Number('0x' + timestamp.substring(0, Math.max(0, timestamp.length - 8)));
     buf.writeUInt32LE(timestampLow, 24, false);
     buf.writeUInt32LE(timestampHigh, 28, false);
     //random client nonce
-    buf.write(nonce || createPseudoRandomValue(16), 32, "hex");
+    buf.write(nonce || createPseudoRandomValue(16), 32, 'hex');
     //zero
     buf.writeUInt32LE(0, 40);
     //complete target information block from type 2 message
@@ -138,15 +99,12 @@ function createNTLMv2Response(type2message, username, ntlmhash, nonce, targetNam
     //zero
     buf.writeUInt32LE(0, 44 + type2message.targetInfo.buffer.length);
     hmac.update(buf.slice(8));
-    let hashedBuffer = hmac.digest();
+    var hashedBuffer = hmac.digest();
     hashedBuffer.copy(buf);
     return buf;
 }
-/**
- * @param length
- */
 function createPseudoRandomValue(length) {
-    let str = "";
+    var str = '';
     while (str.length < length) {
         str += Math.floor(Math.random() * 16).toString(16);
     }
