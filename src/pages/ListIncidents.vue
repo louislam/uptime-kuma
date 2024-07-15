edebe9b7a537955f786d8c73471e0afb7dee334c
Mason Coloretti <mcoloretti7881@outlook.com>
2024-07-14 18:53:49 -0600

    fixed lint format

--- src/pages/ListIncidents.vue
+++ src/pages/ListIncidents.vue
@@ -82,12 +82,6 @@ export default {
     padding: 20px;
     box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
 }
-.jumbotron {
-    background-color: #fff;
-    border-radius: 10px;
-    padding: 20px;
-    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-    margin-bottom: 20px; /* Add spacing between jumbotrons */
-}
+
 </style>
 
