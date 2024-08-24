4ea575c844a3d6c37ca4b1fabaef84cc61a4bc0a
cyril59310 <archas.cyril@hotmail.fr>
2024-08-24 22:29:03 +0200

    Add translation keys

--- src/mixins/socket.js
+++ src/mixins/socket.js
@@ -251,7 +251,7 @@ export default {
 
             socket.on("disconnect", () => {
                 console.log("disconnect");
-                this.connectionErrorMsg = "Lost connection to the socket server. Reconnecting...";
+                this.connectionErrorMsg = `${this.$t("Lost connection to the socket server.")} ${this.$t("Reconnecting...")}`;
                 this.socket.connected = false;
             });
 
