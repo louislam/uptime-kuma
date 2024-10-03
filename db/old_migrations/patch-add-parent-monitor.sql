862dfadbf3d44509a91d368744a0bd20be18cc33
Damyan Petkov <damyanmp@gmail.com>
2024-10-03 00:01:42 -0400

    Adding IF NOT EXIST to the query

--- db/old_migrations/patch-add-parent-monitor.sql
+++ db/old_migrations/patch-add-parent-monitor.sql
@@ -1,7 +1,7 @@
 -- You should not modify if this have pushed to Github, unless it does serious wrong with the db.
 BEGIN TRANSACTION;
 
-ALTER TABLE monitor
-    ADD parent INTEGER REFERENCES [monitor] ([id]) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE monitor 
+    ADD COLUMN IF NOT EXISTS invert_keyword BOOLEAN DEFAULT 0 NOT NULL;
 
 COMMIT;
