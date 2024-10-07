e773e454e9bfcf331095c9f88312bbf7b2e90359
Louis Lam <louislam@users.noreply.github.com>
2024-10-07 19:06:28 +0800

    Fix #5157 and cleanup some devcontainer code (#5162)

--- config/vite.config.js
+++ config/vite.config.js
@@ -16,9 +16,7 @@ export default defineConfig({
     },
     define: {
         "FRONTEND_VERSION": JSON.stringify(process.env.npm_package_version),
-        "DEVCONTAINER": JSON.stringify(process.env.DEVCONTAINER),
-        "GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN": JSON.stringify(process.env.GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN),
-        "CODESPACE_NAME": JSON.stringify(process.env.CODESPACE_NAME),
+        "process.env": {},
     },
     plugins: [
         vue(),
