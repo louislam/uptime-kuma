f9e2982e088a86cbcd2a8c16eb15a2a7e9a3ad38
Erlaan <erlaan@users.noreply.github.com>
2024-10-09 20:17:51 +0200

    This time it may work.... I have no clue what i am doing

--- src/components/NotificationDialog.vue
+++ src/components/NotificationDialog.vue
@@ -118,7 +118,7 @@ export default {
                 "clicksendsms": "ClickSend SMS",
                 "CallMeBot": "CallMeBot (WhatsApp, Telegram Call, Facebook Messanger)",
                 "discord": "Discord",
-                "46elks": "46elks",
+                "46elks": "Elks",
                 "GoogleChat": "Google Chat (Google Workspace)",
                 "gorush": "Gorush",
                 "gotify": "Gotify",
