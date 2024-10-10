40ab5c50b46e0b7ea94914d0619bb740d5ed5185
Erlaan <erlaan@users.noreply.github.com>
2024-10-10 17:59:30 +0200

    This is hard to find all errors i made but maybe i fixed everything this time :)

--- server/notification-providers/46elks.js
+++ server/notification-providers/46elks.js
@@ -21,33 +21,32 @@ class Elks extends NotificationProvider {
             const url = "https://api.46elks.com/a1/sms";
 
             // Request data object
-            var data = {
+            let data = {
                 from: notification.elksFromNumber,
                 to: notification.elksToNumber,
                 message: msg
             };
-          
+
             data = new URLSearchParams(data);
             data = data.toString();
-          
+
             // Set the headers
             const config = {
                 headers: {
                     "Authorization": "Basic " + authKey
                 }
             };
-          
+
             // Send request
             await axios.post(url, data, config);
-          
-            return okMsg;
-        } 
-            catch (error) {
-                this.throwGeneralAxiosError(error);
-            }
 
-    }
-    
+            return okMsg;
+          }
+        catch (error) {
+                this.throwGeneralAxiosError(error);
+        }
+
+    }   
 
 }
 
