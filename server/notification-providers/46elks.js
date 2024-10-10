23192e86178bc0595cbedcae4cfdbb6988296fd2
Erlaan <erlaan@users.noreply.github.com>
2024-10-10 17:22:54 +0200

    Fix the indentationen to 4 spaces

--- server/notification-providers/46elks.js
+++ server/notification-providers/46elks.js
@@ -8,48 +8,48 @@ class Elks extends NotificationProvider {
      * @inheritdoc
      */
     async send(notification, msg, monitorJSON = null, heartbeatJSON = null) {
-      const okMsg = "Sent Successfully.";
+        const okMsg = "Sent Successfully.";
 
 
-      try {
+        try {
 
-        // API credentials
-        const username = notification.elksUsername;
-        const password = notification.elksAuthToken;
-        const authKey  = Buffer.from(username + ":" + password).toString("base64");
-    
-        // Set the SMS endpoint
-        const url = "https://api.46elks.com/a1/sms";
-    
-        // Request data object
-        var data = {
-          from: notification.elksFromNumber,
-          to: notification.elksToNumber,
-          message: msg
-        }
-    
-        data = new URLSearchParams(data);
-        data = data.toString();
-    
-        // Set the headers
-        const config = {
-          headers: {
-            "Authorization": "Basic " + authKey
-          }
-        };
-    
-        // Send request
-        await axios.post(url, data, config);
-        //const res = await axios.post(url, data, config);
-        //console.log(res.data);
-    
-        return okMsg;
+            // API credentials
+            const username = notification.elksUsername;
+            const password = notification.elksAuthToken;
+            const authKey  = Buffer.from(username + ":" + password).toString("base64");
+
+           // Set the SMS endpoint
+            const url = "https://api.46elks.com/a1/sms";
+
+           // Request data object
+            var data = {
+              from: notification.elksFromNumber,
+              to: notification.elksToNumber,
+              message: msg
+            }
+          
+           data = new URLSearchParams(data);
+            data = data.toString();
+          
+           // Set the headers
+            const config = {
+              headers: {
+                "Authorization": "Basic " + authKey
+              }
+            };
+          
+           // Send request
+            await axios.post(url, data, config);
+            //const res = await axios.post(url, data, config);
+            //console.log(res.data);
+          
+           return okMsg;
         } 
-        catch (error) {
-            this.throwGeneralAxiosError(error);
-        }
-        
-      }
+            catch (error) {
+                this.throwGeneralAxiosError(error);
+            }
+
+    }
     
 
 }
