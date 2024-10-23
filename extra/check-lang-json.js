61a61a555577ab4b6a62255884c1aa400b7ffe57
Louis Lam <louislam@users.noreply.github.com>
2024-10-23 08:56:35 +0800

    Test

---
+++ extra/check-lang-json.js
@@ -0,0 +1,25 @@
+// For #5231
+
+const fs = require("fs");
+
+let path = "../src/lang";
+
+// list directories in the lang directory
+let jsonFileList = fs.readdirSync(path);
+
+for (let jsonFile of jsonFileList) {
+    if (!jsonFile.endsWith(".json")) {
+        continue;
+    }
+
+    let jsonPath = path + "/" + jsonFile;
+    let originalContent = fs.readFileSync(jsonPath, "utf8");
+    let langData = JSON.parse(originalContent);
+
+    let formattedContent = JSON.stringify(langData, null, 4) + "\n";
+
+    if (originalContent !== formattedContent) {
+        console.error(`File ${jsonFile} is not formatted correctly.`);
+        process.exit(1);
+    }
+}
