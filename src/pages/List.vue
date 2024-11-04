c62f0d7bd6c53f40224ce7997221bc9b69b51141
Logorrheique <rheiquelogor@gmail.com>
2024-11-04 13:25:12 +0100

    Revert "fix double quote"
    This reverts commit c2f77476951c80c15897cde232b549d3e6f6defb.

--- src/pages/List.vue
+++ src/pages/List.vue
@@ -12,19 +12,21 @@ export default {
         MonitorList,
     },
     mounted() {
-        if (!this.$root.isMobile && this.$route.path === "/list") {
-            this.$router.push("dashboard");
+        if (!this.$root.isMobile && this.$route.path === '/list') {
+            this.$router.push('/dashboard');
         }
     },
     watch: {
-        "$root.isMobile"(newVal) {
-            if (!newVal && this.$route.path === "/list") {
-                this.$router.push("/dashboard");
+        '$root.isMobile'(newVal) {
+            if (!newVal && this.$route.path === '/list') {
+                this.$router.push('/dashboard');
             }
         }
     }
 };
 </script>
+
+<style lang="scss" scoped>
 @import "../assets/vars";
 
 .shadow-box {
