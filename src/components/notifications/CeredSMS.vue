7c67daf7ccc9ff24f47c6c11d12099d591d0fd10
czarek <kontakt@cered.pl>
2024-11-21 23:50:08 +0100

    lang fix

--- src/components/notifications/CeredSMS.vue
+++ src/components/notifications/CeredSMS.vue
@@ -4,11 +4,11 @@
         <HiddenInput id="ceredsms-key" v-model="$parent.notification.ceredsmsApiKey" :required="true" autocomplete="new-password"></HiddenInput>
     </div>
     <div class="mb-3">
-        <label for="ceredsms-phone-number" class="form-label">{{ $t("ceredsmsPhoneNumber") }}</label>
+        <label for="ceredsms-phone-number" class="form-label">{{ $t("To Phone Number") }}</label>
         <input id="ceredsms-phone-number" v-model="$parent.notification.ceredsmsPhoneNumber" type="text" class="form-control" required>
     </div>
     <div class="mb-3">
-        <label for="ceredsms-sender-name" class="form-label">{{ $t("ceredsmsSenderName") }}</label>
+        <label for="ceredsms-sender-name" class="form-label">{{ $t("Originator") }}</label>
         <input id="ceredsms-sender-name" v-model="$parent.notification.ceredsmsSenderName" type="text" minlength="3" maxlength="11" class="form-control">
     </div>
 </template>
