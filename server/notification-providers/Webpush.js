4ae1d0906cae3e492a9266b045fe6406fc451a75
Connor <cmail9@yahoo.com>
2024-12-16 14:21:39 -0500

    Lint cleanup and comments
    (cherry picked from commit 1041812645d199f54cb8c22c115ef2bd267ccaa7 by DoidoYo <gabrielblfernandes@gmail.com>)

--- server/notification-providers/Webpush.js
+++ server/notification-providers/Webpush.js
@@ -1,7 +1,6 @@
 const NotificationProvider = require("./notification-provider");
-const axios = require("axios");
 const { UP } = require("../../src/util");
-const web_push = require('web-push');
+const webpush = require("web-push");
 const { setting } = require("../util-server");
 
 class Webpush extends NotificationProvider {
@@ -18,31 +17,31 @@ class Webpush extends NotificationProvider {
             const publicVapidKey = await setting("webpushPublicVapidKey");
             const privateVapidKey = await setting("webpushPrivateVapidKey");
             // Set Vapid keys in web-push helper lib
-            web_push.setVapidDetails("https://github.com/louislam/uptime-kuma", publicVapidKey, privateVapidKey);
-            
+            webpush.setVapidDetails("https://github.com/louislam/uptime-kuma", publicVapidKey, privateVapidKey);
+
             if (heartbeatJSON === null && monitorJSON === null) {
                 // Test message
                 const data = JSON.stringify({
-                    title: "TEST", 
+                    title: "TEST",
                     body: "Test Alert - " + msg
                 });
                 //send push notification using web-push lib
-                await web_push.sendNotification(notification.subscription, data);
-                
+                await webpush.sendNotification(notification.subscription, data);
+
                 return okMsg;
             }
 
             const title = `Monitor ${heartbeatJSON["status"] === UP ? "UP" : "DOWN"}`;
             const down = "❌ " + monitorJSON["name"] + " is DOWN ❌";
-            const up = "✅ " + monitorJSON["name"] + " is UP ✅";;
+            const up = "✅ " + monitorJSON["name"] + " is UP ✅";
 
-            const data = JSON.stringify({ 
-                title: title, 
+            const data = JSON.stringify({
+                title: title,
                 body: `${heartbeatJSON["status"] === UP ? up : down}`
             });
             //send push notification using web-push lib
-            await web_push.sendNotification(notification.subscription, data);
-            
+            await webpush.sendNotification(notification.subscription, data);
+
             return okMsg;
         } catch (error) {
             this.throwGeneralAxiosError(error);
