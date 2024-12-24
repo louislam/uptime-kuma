ddf35ec96f1b55697c940a8dbe794f0f7834673a
Manthan Ankolekar <manthan.ank46@gmail.com>
2024-12-24 11:33:02 +0530

    Fix list route for responsive UI
    Related to #359
    
    Add a check to redirect to the homepage if the view is not mobile in the `src/pages/List.vue` file.
    
    * Import `mobileMixin` in `src/pages/List.vue`.
    * Add a `created` lifecycle hook to check if the view is mobile and redirect to `/dashboard` if not.
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/louislam/uptime-kuma/issues/359?shareId=XXXX-XXXX-XXXX-XXXX).

--- src/pages/List.vue
+++ src/pages/List.vue
@@ -6,10 +6,17 @@
 
 <script>
 import MonitorList from "../components/MonitorList.vue";
+import mobileMixin from "../mixins/mobile.js";
 
 export default {
     components: {
         MonitorList,
+    },
+    mixins: [mobileMixin],
+    created() {
+        if (!this.isMobile) {
+            this.$router.push("/dashboard");
+        }
     },
 };
 </script>
