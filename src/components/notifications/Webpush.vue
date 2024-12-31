8ee7a6d0b4d4bf5cebda22182931a9539de9858b
Connor <cmail9@yahoo.com>
2024-12-31 14:34:58 -0500

    Addressing CommanderStorm code comments
    (cherry picked from commit af301cd3f2cf6806e6d0bb14a8c69dc0c60290ee by DoidoYo <gabrielblfernandes@gmail.com>)

--- src/components/notifications/Webpush.vue
+++ src/components/notifications/Webpush.vue
@@ -3,15 +3,18 @@
         class="mb-3"
         type="button" :class="[
             'btn',
-            canRegister ? 'btn-primary' : 'btn-danger'
-        ]" :disabled="!btnEnabled" @click="registerWebpush">
+            browserSupportsServiceWorkers ? 'btn-primary' : 'btn-danger'
+        ]"
+        :disabled="!btnEnabled"
+        @click="registerWebpush"
+    >
         <div v-if="processing" class="spinner-border spinner-border-sm me-1"></div>
         <span v-else-if="$parent.notification.subscription" class="me-1">✓</span>
         {{ btnText }}
     </button>
 
-    <div class="mb-3 form-text">
-        <a href="TODO" target="_blank">{{ $t("documentationOf", ["Webpush"]) }}</a>
+    <div class="form-text">
+        {{ $t("Webpush Helptext") }}
     </div>
 </template>
 
@@ -19,31 +22,26 @@
 export default {
     data() {
         return {
-            //store subscription info
             btnEnabled: false,
             btnText: "",
             processing: false,
-            //determines if browser supports service worker
-            canRegister: false,
-            //store public vapid key
+            browserSupportsServiceWorkers: false,
             publicVapidKey: null,
         };
     },
     mounted() {
-        // if already subscribed
         if (this.$parent.notification.subscription) {
             this.btnEnabled = false;
-            this.canRegister = true;
-            this.btnText = "Notifications Enabled";
-        } else { //not subscribed
-            //check if browser supports service worker
+            this.browserSupportsServiceWorkers = true;
+            this.btnText = this.$t("Notifications Enabled");
+        } else {
             if (("serviceWorker" in navigator)) {
-                this.btnText = "Allow Notifications";
-                this.canRegister = true;
+                this.btnText = this.$t("Allow Notifications");
+                this.browserSupportsServiceWorkers = true;
                 this.btnEnabled = true;
-            } else { //browser does not support service worker
-                this.btnText = "Browser not supported";
-                this.canRegister = false;
+            } else {
+                this.btnText = this.$t("Browser not supported");
+                this.browserSupportsServiceWorkers = false;
                 this.btnEnabled = false;
             }
         }
@@ -53,40 +51,37 @@ export default {
             this.processing = true;
 
             try {
-                // Get the VAPID public key from the server
                 const publicKey = await new Promise((resolve, reject) => {
                     this.$root.getSocket().emit("getWebpushVapidPublicKey", (resp) => {
                         if (!resp.ok) {
                             reject(new Error(resp.msg));
                         }
+                        console.log(resp.msg);
                         resolve(resp.msg);
                     });
                 });
 
-                //request permission to send notifications
                 const permission = await Notification.requestPermission();
                 if (permission !== "granted") {
                     this.$root.toastRes({
                         ok: false,
-                        msg: "Unable to get permission to notify.",
+                        msg: this.$t("Unable to get permission to notify"),
                     });
                     this.processing = false;
                     return;
                 }
 
-                //get service worker registration
                 const registration = await navigator.serviceWorker.ready;
-                //subscribe to push notifications
+
                 const subscription = await registration.pushManager.subscribe({
                     userVisibleOnly: true,
                     applicationServerKey: publicKey,
                 });
 
-                //store subscription info and update button
                 this.$parent.notification.subscription = subscription;
                 this.btnEnabled = false;
-                this.canRegister = true;
-                this.btnText = "Notifications Enabled";
+                this.browserSupportsServiceWorkers = true;
+                this.btnText = this.$t("Notifications Enabled");
             } catch (error) {
                 console.error("Subscription failed:", error);
                 this.$root.toastRes({
