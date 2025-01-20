a104a6eb808d0e2caec347f8cde34497cbf5fdea
DayShift <2922897389@qq.com>
2025-01-20 20:02:39 +0800

    4 space

--- test/backend-test/test-apicache-ReDos.js
+++ test/backend-test/test-apicache-ReDos.js
@@ -11,16 +11,16 @@ const apicacheModule = require("../../server/modules/apicache/apicache.js");
 const assert = require("node:assert");
 
 test("Test ReDos - attack string", async (t) => {
-  const getDuration = apicacheModule.getDuration;
-  const str = "" + "00".repeat(100000) + "\u0000";
-  const startTime = performance.now();
-  try {
-    getDuration(str);
-  } catch (error) {
+    const getDuration = apicacheModule.getDuration;
+    const str = "" + "00".repeat(100000) + "\u0000";
+    const startTime = performance.now();
+    try {
+        getDuration(str);
+    } catch (error) {
     // pass
-  }
-  const endTime = performance.now();
-  const elapsedTime = endTime - startTime;
-  const reDosThreshold = 9000;
-  assert(elapsedTime <= reDosThreshold, `🚨 Potential ReDoS Attack! getDuration method took ${elapsedTime.toFixed(2)} ms, exceeding threshold of ${reDosThreshold} ms.`);
+    }
+    const endTime = performance.now();
+    const elapsedTime = endTime - startTime;
+    const reDosThreshold = 9000;
+    assert(elapsedTime <= reDosThreshold, `🚨 Potential ReDoS Attack! getDuration method took ${elapsedTime.toFixed(2)} ms, exceeding threshold of ${reDosThreshold} ms.`);
 });
