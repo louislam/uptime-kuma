2d46a3243121d387ee7f4e932cfa38e6d6866f66
Your Name <you@example.com>
2025-02-17 19:54:48 -0800

    skip insecure test except generic arm64

--- test/backend-test/test-websocket.js
+++ test/backend-test/test-websocket.js
@@ -42,7 +42,7 @@ describe("Websocket Test", {
     });
 
     test("Insecure Websocket", {
-        skip: !!process.env.CI && process.arch === "x64" && (process.platform === "darwin" || process.platform === "linux" || process.platform === "win32"),
+        skip: !!process.env.CI && process.arch !== "x64" && process.platform !== "linux",
     }, async () => {
         const websocketMonitor = new websocket();
 
