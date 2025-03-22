0b146e27c42d37a54abeb05edc654b60706d1083
Teror Fox <i@trfox.top>
2025-03-22 16:13:58 +0800

    feat(notifications): add link for pushplus docs

--- src/components/notifications/PushPlus.vue
+++ src/components/notifications/PushPlus.vue
@@ -3,6 +3,7 @@
         <label for="pushPlus-sendkey" class="form-label">{{ $t("SendKey") }}</label>
         <HiddenInput id="pushPlus-sendkey" v-model="$parent.notification.pushPlusSendKey" :required="true" autocomplete="new-password"></HiddenInput>
     </div>
+    <a href="https://www.pushplus.plus/" target="_blank">{{ $t("LearnMore") }}</a>
 </template>
 
 <script>
