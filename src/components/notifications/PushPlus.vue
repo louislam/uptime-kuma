674d2b2424325e5ab68da5ff1d7c63845582f2e1
Teror Fox <i@trfox.top>
2025-03-26 23:12:34 +0800

    Update src/components/notifications/PushPlus.vue
    Co-authored-by: Frank Elsinga <frank@elsinga.de>

--- src/components/notifications/PushPlus.vue
+++ src/components/notifications/PushPlus.vue
@@ -3,7 +3,9 @@
         <label for="pushPlus-sendkey" class="form-label">{{ $t("SendKey") }}</label>
         <HiddenInput id="pushPlus-sendkey" v-model="$parent.notification.pushPlusSendKey" :required="true" autocomplete="new-password"></HiddenInput>
     </div>
-    <a href="https://www.pushplus.plus/" target="_blank">LearnMore"</a>
+    <i18n-t tag="div" keypath="More info on:" class="mb-3 form-text">
+      <a href="https://www.pushplus.plus/" target="_blank">https://www.pushplus.plus/</a>
+    </i18n-t>
 </template>
 
 <script>
