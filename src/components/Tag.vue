8d8e3e5a8e78030b8ffbe258dc872b8bea234390
Jonathan Starck <29304143+Psycho0verload@users.noreply.github.com>
2025-04-19 01:34:16 +0200

    fix(dashboard): prevent y-overlapping of tags by adding a gap #5773 (#5774)
    Co-authored-by: Frank Elsinga <frank@elsinga.de>

--- src/components/Tag.vue
+++ src/components/Tag.vue
@@ -6,7 +6,6 @@
                   'm-2': size == 'normal',
                   'px-2': size == 'sm',
                   'py-0': size == 'sm',
-                  'mx-1': size == 'sm',
         }"
         :style="{ backgroundColor: item.color, fontSize: size == 'sm' ? '0.7em' : '1em' }"
     >
