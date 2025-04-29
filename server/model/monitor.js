326a996a4851b02ee92182ac55ee93349d5aad78
Riccardo Crippa <therickys93@gmail.com>
2025-04-29 15:55:02 +0200

    fix linting.

--- server/model/monitor.js
+++ server/model/monitor.js
@@ -1287,9 +1287,9 @@ class Monitor extends BeanModel {
             let text;
             if (bean.status === UP) {
                 text = "✅ Up";
-            } else if(bean.status === DOWN) {
+            } else if (bean.status === DOWN) {
                 text = "🔴 Down";
-            } else if(bean.status === MAINTENANCE) {
+            } else if (bean.status === MAINTENANCE) {
                 text = "🟦 Maintenance";
             }
 
