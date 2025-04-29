459d34c6bc8d185b3eaa8b3210691b857db72d88
Riccardo Crippa <therickys93@gmail.com>
2025-04-29 15:27:29 +0200

    update maintenance icon on notification.

--- server/model/monitor.js
+++ server/model/monitor.js
@@ -1290,7 +1290,7 @@ class Monitor extends BeanModel {
             } else if(bean.status === DOWN) {
                 text = "🔴 Down";
             } else if(bean.status === MAINTENANCE) {
-                text = "🔴 Maintenance";
+                text = "🟦 Maintenance";
             }
 
             let msg = `[${monitor.name}] [${text}] ${bean.msg}`;
