483477389c89957cbcfafa6be6862df8883ddde6
Riccardo Crippa <therickys93@gmail.com>
2025-05-05 07:44:03 +0200

    Revert "change Maintenance notification text."
    This reverts commit 9cbbed4bbedead66bda2f883f68cca30e9dd9580.

--- server/model/monitor.js
+++ server/model/monitor.js
@@ -1270,7 +1270,6 @@ class Monitor extends BeanModel {
             (previousBeatStatus === DOWN && currentBeatStatus === MAINTENANCE) ||
             (previousBeatStatus === UP && currentBeatStatus === DOWN) ||
             (previousBeatStatus === DOWN && currentBeatStatus === UP) ||
-            (previousBeatStatus === PENDING && currentBeatStatus === MAINTENANCE) ||
             (previousBeatStatus === PENDING && currentBeatStatus === DOWN);
     }
 
@@ -1291,7 +1290,7 @@ class Monitor extends BeanModel {
             } else if (bean.status === DOWN) {
                 text = "🔴 Down";
             } else if (bean.status === MAINTENANCE) {
-                text = "Under Maintenance";
+                text = "🔵 Maintenance";
             }
 
             let msg = `[${monitor.name}] [${text}] ${bean.msg}`;
