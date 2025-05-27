4beccbf4e3557f43e929777f3689922247354f9d
guguji5 <604821528@qq.com>
2025-05-27 14:37:52 +0800

    Flashduty IntegrationKey support Endpoint URL

--- src/components/notifications/FlashDuty.vue
+++ src/components/notifications/FlashDuty.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="mb-3">
-        <label for="flashduty-integration-url" class="form-label">Integration Key</label>
-        <HiddenInput id="flashduty-integration-url" v-model="$parent.notification.flashdutyIntegrationKey" autocomplete="false"></HiddenInput>
+        <label for="flashduty-integration-url" class="form-label">{{ $t("FlashDuty Push URL") }} <span style="color: red;"><sup>*</sup></span></label>
+        <input id="flashduty-integration-url" v-model="$parent.notification.flashdutyIntegrationKey" type="text" class="form-control" required></input>
+        <div class="form-text">
+            <p><span style="color: red;"><sup>*</sup></span>{{ $t("Required") }}</p>
+        </div>
         <i18n-t tag="div" keypath="wayToGetFlashDutyKey" class="form-text">
             <a href="https://flashcat.cloud/product/flashduty?from=kuma" target="_blank">{{ $t("here") }}</a>
         </i18n-t>
@@ -17,12 +20,8 @@
 </template>
 
 <script>
-import HiddenInput from "../HiddenInput.vue";
 
 export default {
-    components: {
-        HiddenInput,
-    },
     mounted() {
     }
 };
