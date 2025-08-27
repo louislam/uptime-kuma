69f93cdedf421f5c8f3b3021a388132e9436276d
Frank Elsinga <frank@elsinga.de>
2025-08-27 03:19:22 +0200

    Update src/pages/DashboardHome.vue

--- src/pages/DashboardHome.vue
+++ src/pages/DashboardHome.vue
@@ -254,9 +254,9 @@ export default {
                 this.$root.toastSuccess(this.$t("Events cleared successfully"));
             } else {
                 this.$root.toastError(
-                    this.$t("Could not clear events", { 
-                       failed,
-                       total,
+                    this.$t("Could not clear events", {
+                        failed,
+                        total,
                     })
                 );
             }
