49e3ebb5d6b5f90c98578304056a5deed0b1b301
Frank Elsinga <frank@elsinga.de>
2025-09-21 18:45:26 +0200

    Update src/components/Login.vue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -16,7 +16,7 @@
 
                 <div v-if="tokenRequired">
                     <div class="form-floating mt-3">
-                        <input ref="otpInput" id="otp" v-model="token" type="text" maxlength="6" class="form-control" placeholder="123456" autocomplete="one-time-code" required>
+                        <input id="otp" ref="otpInput" v-model="token" type="text" maxlength="6" class="form-control" placeholder="123456" autocomplete="one-time-code" required>
                         <label for="otp">{{ $t("Token") }}</label>
                     </div>
                 </div>
