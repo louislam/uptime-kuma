cf916a6abf67217dd4a37a74e9b614fd5cb4e58c
Radu Lucuț <git@radulucut.com>
2025-10-14 16:46:03 +0300

    update DNS message

--- server/monitor-types/globalping.js
+++ server/monitor-types/globalping.js
@@ -342,7 +342,7 @@ class GlobalpingMonitorType extends MonitorType {
         let conditionsResult = true;
         const handleConditions = (data) => conditions ? evaluateExpressionGroup(conditions, data) : true;
 
-        const dnsMessage = (result.answers || []).map(answer => `${answer.name} ${answer.type} ${answer.ttl} ${answer.class} ${answer.value}`).join(" | ");
+        const dnsMessage = (result.answers || []).map(answer => answer.value).join(" | ");
         const values = (result.answers || []).map(answer => answer.value);
 
         switch (monitor.dns_resolve_type) {
@@ -371,6 +371,9 @@ class GlobalpingMonitorType extends MonitorType {
         }
 
         heartbeat.ping = result.timings.total || 0;
+        if (!dnsMessage) {
+            this.dnsMessage = "no records found";
+        }
         heartbeat.msg = this.formatResponse(probe, dnsMessage);
         heartbeat.status = conditionsResult ? UP : DOWN;
     }
