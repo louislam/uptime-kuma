a8351c8f9280a866d551783a8daa6c6feabc284b
Louis Lam <louislam@users.noreply.github.com>
2025-11-26 00:28:56 +0800

    Add manual test

---
+++ test/manual-test-grpc/simple-grpc-server.js
@@ -0,0 +1,20 @@
+const grpc = require("@grpc/grpc-js");
+const protoLoader = require("@grpc/proto-loader");
+const packageDef = protoLoader.loadSync("echo.proto", {});
+const grpcObject = grpc.loadPackageDefinition(packageDef);
+const { echo } = grpcObject;
+
+/**
+ * @param call
+ * @param callback
+ */
+function Echo(call, callback) {
+    callback(null, { message: call.request.message });
+}
+
+const server = new grpc.Server();
+server.addService(echo.EchoService.service, { Echo });
+server.bindAsync("0.0.0.0:50051", grpc.ServerCredentials.createInsecure(), () => {
+    console.log("gRPC server running on :50051");
+    server.start();
+});
