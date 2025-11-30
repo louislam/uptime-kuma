72e72bb394799bfd3335cfa6fec35dcf72257d27
Shaan <mail@shaan.icu>
2025-11-30 12:17:20 +0600

    Revert "Add .de RDAP server"
    This reverts commit 622c00586ffa5d04fed913307b672ed8c29dc0ac.
    denic does not support expiry date

--- server/model/domain_expiry.js
+++ server/model/domain_expiry.js
@@ -21,10 +21,6 @@ const cachedFetch = process.env.NODE_ENV ? NodeFetchCache.create({
  * @returns {Promise<string>} First RDAP server found
  */
 async function getRdapServer(tld) {
-    switch (tld) {
-        case "de": // not present in iana.org json
-            return "https://rdap.denic.de/domain/";
-    }
     let rdapList;
     try {
         const res = await cachedFetch("https://data.iana.org/rdap/dns.json");
