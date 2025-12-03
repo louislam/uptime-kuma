0d8624ccd034f98d927e4105dd6f4a182d62b2fd
Dalton Pearson <dalton.pearson@praemo.com>
2025-12-03 12:53:48 -0500

    inlined postgresQuery

--- server/monitor-types/postgres.js
+++ server/monitor-types/postgres.js
@@ -2,6 +2,8 @@ const { MonitorType } = require("./monitor-type");
 const { UP } = require("../../src/util");
 const dayjs = require("dayjs");
 const { postgresQuery } = require("../util-server");
+const postgresConParse = require("pg-connection-string").parse;
+const { Client } = require("pg");
 
 class PostgresMonitorType extends MonitorType {
     name = "postgres";
@@ -12,10 +14,57 @@ class PostgresMonitorType extends MonitorType {
     async check(monitor, heartbeat, _server) {
         let startTime = dayjs().valueOf();
 
-        await postgresQuery(
-            monitor.databaseConnectionString,
-            monitor.databaseQuery || "SELECT 1"
-        );
+        await new Promise((resolve, reject) => {
+            const connectionString = monitor.databaseConnectionString;
+            let query = monitor.databaseQuery || "SELECT 1";
+
+            const config = postgresConParse(connectionString);
+
+            // Fix #3868: ensure SSL string values become booleans
+            if (typeof config.ssl === "string") {
+                config.ssl = config.ssl === "true";
+            }
+
+            if (config.password === "") {
+                reject(new Error("Password is undefined."));
+                return;
+            }
+
+            const client = new Client(config);
+
+            client.on("error", (error) => {
+                log.debug("postgres", "Error caught in error event handler.");
+                reject(error);
+            });
+
+            client.connect((err) => {
+                if (err) {
+                    reject(err);
+                    client.end();
+                } else {
+                    try {
+                        if (
+                            !query ||
+                            (typeof query === "string" && query.trim() === "")
+                        ) {
+                            query = "SELECT 1";
+                        }
+
+                        client.query(query, (err, res) => {
+                            if (err) {
+                                reject(err);
+                            } else {
+                                resolve(res);
+                            }
+                            client.end();
+                        });
+                    } catch (e) {
+                        reject(e);
+                        client.end();
+                    }
+                }
+            });
+        });
 
         heartbeat.msg = "";
         heartbeat.status = UP;
