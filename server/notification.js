ba02c70d321e739bca6647948b11b17af7859f89
John Huseinovic <john@huseinovic.net>
2025-12-04 03:59:15 +0000

    MobivateSMS Notification Provider support

--- server/notification.js
+++ server/notification.js
@@ -29,6 +29,7 @@ const LineNotify = require("./notification-providers/linenotify");
 const LunaSea = require("./notification-providers/lunasea");
 const Matrix = require("./notification-providers/matrix");
 const Mattermost = require("./notification-providers/mattermost");
+const MobivateSMS = require("./notification-providers/mobivatesms");
 const NextcloudTalk = require("./notification-providers/nextcloudtalk");
 const Nostr = require("./notification-providers/nostr");
 const Ntfy = require("./notification-providers/ntfy");
---
+++ src/components/notifications/MobivateSMS.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="mb-3">
+        <label for="mobivate-key" class="form-label">{{ $t("API Key") }}</label>
+        <HiddenInput id="mobivate-key" v-model="$parent.notification.mobivateApikey" :required="true" autocomplete="new-password"></HiddenInput>
+        <div class="form-text">
+            <i18n-t keypath="mobivateApiurl" tag="div" class="form-text">
+                <a href="https://hub.mobivate.com" target="_blank">hub.mobivate.com</a>
+            </i18n-t>
+        </div>
+    </div>
+    <div class="mb-3">
+        <label for="mobivate-recipients" class="form-label">{{ $t("mobivateRecipients") }}</label>
+        <input id="mobivate-recipients" v-model="$parent.notification.mobivateRecipients" type="text" minlength="3" maxlength="20" pattern="^[\d+,]+$" class="form-control" required>
+        <div class="form-text">
+            <i18n-t keypath="mobivateRecipientsHelptext" tag="div" class="form-text">
+                <code>+447xxxxxxx1</code>
+                <code>+447xxxxxxx2</code>
+                <code>,</code>
+            </i18n-t>
+        </div>
+    </div>
+    <div class="mb-3">
+        <label for="mobivate-originator" class="form-label">{{ $t("mobivateOriginator") }}</label>
+        <input id="mobivate-originator" v-model="$parent.notification.mobivateOriginator" type="text" minlength="3" maxlength="15" pattern="^[a-zA-Z0-9]*$" class="form-control" required>
+        <div class="form-text">
+            {{ $t("mobivateOriginatorInfo") }}
+        </div>
+    </div>
+</template>
+
+<script>
+import HiddenInput from "../HiddenInput.vue";
+
+export default {
+    components: {
+        HiddenInput,
+    },
+};
+</script>
