7d0a7062e6ff25be8e3b987eb29b41f76551c3ea
John Huseinovic <john@huseinovic.net>
2025-12-04 04:47:53 +0000

    added text

--- src/components/notifications/MobivateSMS.vue
+++ src/components/notifications/MobivateSMS.vue
@@ -4,11 +4,14 @@
         <HiddenInput id="mobivate-key" v-model="$parent.notification.mobivateApikey" :required="true" autocomplete="new-password"></HiddenInput>
     </div>
     <div class="mb-3">
-        <label for="mobivate-recipients" class="form-label">{{ $t("mobivateRecipients") }}</label>
+        <label for="mobivate-recipients" class="form-label">{{ $t("Recipients") }}</label>
         <input id="mobivate-recipients" v-model="$parent.notification.mobivateRecipients" type="text" minlength="3" maxlength="20" pattern="^[\d+,]+$" class="form-control" required>
+        <div class="form-text">
+            <p>{{ $t("Comma separated list of numbers in international format. (eg. 447930000000,447930000001)") }}</p>
+        </div>
     </div>
     <div class="mb-3">
-        <label for="mobivate-originator" class="form-label">{{ $t("mobivateOriginator") }}</label>
+        <label for="mobivate-originator" class="form-label">{{ $t("Originator") }}</label>
         <input id="mobivate-originator" v-model="$parent.notification.mobivateOriginator" type="text" minlength="3" maxlength="15" pattern="^[a-zA-Z0-9]*$" class="form-control" required>
     </div>
 </template>
