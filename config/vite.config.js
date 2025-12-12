8e93a1d4da39a7d392f2064ebb2f90b520afad95
iotux <eigilb@gmail.com>
2025-12-12 09:31:12 +0100

    Removed temporary block from vite.config.js

--- config/vite.config.js
+++ config/vite.config.js
@@ -13,13 +13,6 @@ const viteCompressionFilter = /\.(js|mjs|json|css|html|svg)$/i;
 export default defineConfig({
     server: {
         port: 3000,
-        watch: {
-            ignored: [
-                "**/node_modules/**",
-                "**/dist/**",
-                "**/data/**",
-            ],
-        },
     },
     define: {
         "FRONTEND_VERSION": JSON.stringify(process.env.npm_package_version),
