453e468b77a0ab24861dbf8aacc4fe9a967892fc
Frank Elsinga <frank@elsinga.de>
2026-01-01 16:42:56 +0100

    refactor the system utilities test

--- test/backend-test/test-util.js
+++ test/backend-test/test-util.js
@@ -1,18 +1,26 @@
-const test = require("node:test");
+const { describe, test } = require("node:test");
 const assert = require("node:assert");
 
 const { getDaysRemaining, getDaysBetween } = require("../../server/util-server");
 
-test("Test getDaysBetween", async (t) => {
-    let days = getDaysBetween(new Date(2025, 9, 7), new Date(2025, 9, 10));
-    assert.strictEqual(days, 3);
-    days = getDaysBetween(new Date(2024, 9, 7), new Date(2025, 9, 10));
-    assert.strictEqual(days, 368);
-});
+describe("Server Utilities", () => {
+    test("getDaysBetween() calculates days between dates within same month", () => {
+        const days = getDaysBetween(new Date(2025, 9, 7), new Date(2025, 9, 10));
+        assert.strictEqual(days, 3);
+    });
 
-test("Test getDaysRemaining", async (t) => {
-    let days = getDaysRemaining(new Date(2025, 9, 7), new Date(2025, 9, 10));
-    assert.strictEqual(days, 3);
-    days = getDaysRemaining(new Date(2025, 9, 10), new Date(2025, 9, 7));
-    assert.strictEqual(days, -3);
+    test("getDaysBetween() calculates days between dates across years", () => {
+        const days = getDaysBetween(new Date(2024, 9, 7), new Date(2025, 9, 10));
+        assert.strictEqual(days, 368);
+    });
+
+    test("getDaysRemaining() returns positive value when target date is in future", () => {
+        const days = getDaysRemaining(new Date(2025, 9, 7), new Date(2025, 9, 10));
+        assert.strictEqual(days, 3);
+    });
+
+    test("getDaysRemaining() returns negative value when target date is in past", () => {
+        const days = getDaysRemaining(new Date(2025, 9, 10), new Date(2025, 9, 7));
+        assert.strictEqual(days, -3);
+    });
 });
