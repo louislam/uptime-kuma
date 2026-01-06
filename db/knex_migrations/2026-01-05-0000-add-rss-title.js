94e9005002f860bb9ce1ac5650dec5e136f3c4cf
leonace924 <meobius123@gmail.com>
2026-01-05 20:12:37 -0500

    fix: update the migration js

--- db/knex_migrations/2026-01-05-0000-add-rss-title.js
+++ db/knex_migrations/2026-01-05-0000-add-rss-title.js
@@ -1,8 +1,11 @@
-exports.up = function (knex) {
-    return knex.schema
-        .alterTable("status_page", function (table) {
+exports.up = async function (knex) {
+    // Check if column already exists (created by knex_init_db.js for fresh installs)
+    const hasColumn = await knex.schema.hasColumn("status_page", "rss_title");
+    if (!hasColumn) {
+        await knex.schema.alterTable("status_page", function (table) {
             table.string("rss_title", 255);
         });
+    }
 };
 
 exports.down = function (knex) {
