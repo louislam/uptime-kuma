8827f14343340f185137c0aff641aba8688e6a09
Louis Lam <louislam@users.noreply.github.com>
2026-01-07 17:27:09 +0800

    WIP

--- test/backend-test/test-migration.js
+++ test/backend-test/test-migration.js
@@ -19,13 +19,9 @@ describe("Database Migration", () => {
             fs.unlinkSync(testDbPath);
         }
 
-        // Use the same SQLite driver as the project
-        const Dialect = require("knex/lib/dialects/sqlite3/index.js");
-        Dialect.prototype._driver = () => require("@louislam/sqlite3");
-
         const knex = require("knex");
         const db = knex({
-            client: Dialect,
+            client: "better-sqlite3",
             connection: {
                 filename: testDbPath
             },
