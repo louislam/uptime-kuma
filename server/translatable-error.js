1db60ca5cbfc9f142fc14bf6087aa1dcc4497dab
Frank Elsinga <frank@elsinga.de>
2026-01-08 02:25:19 +0100

    simplify TranslatableError

--- server/translatable-error.js
+++ server/translatable-error.js
@@ -1,16 +1,19 @@
+/**
+ * Error whose message is a translation key.
+ * @augments Error
+ */
 class TranslatableError extends Error {
     /**
-     * Error whose message is a translation key.
-     * @augments Error
+     * Indicates that the error message is a translation key.
      */
+    static msgi18n = true;
+
     /**
      * Create a TranslatableError.
      * @param {string} key - Translation key present in src/lang/en.json
      */
     constructor(key) {
         super(key);
-        this.msgi18n = true;
-        this.key = key;
         Error.captureStackTrace(this, this.constructor);
     }
 }
