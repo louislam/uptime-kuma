b0599b3998837bf19fb8b9b8522371201b233474
Frank Elsinga <frank@elsinga.de>
2026-01-13 02:10:04 +0100

    Apply suggestion from @CommanderStorm

--- db/knex_migrations/2025-10-15-0001-add-monitor-response-config.js
+++ db/knex_migrations/2025-10-15-0001-add-monitor-response-config.js
@@ -2,7 +2,7 @@ exports.up = function (knex) {
     return knex.schema.alterTable("monitor", function (table) {
         table.boolean("save_response").notNullable().defaultTo(false);
         table.boolean("save_error_response").notNullable().defaultTo(true);
-        table.integer("response_max_length").notNullable().defaultTo(256); // Default 256B
+        table.integer("response_max_length").notNullable().defaultTo(1024); // Default 1KB
     });
 };
 
