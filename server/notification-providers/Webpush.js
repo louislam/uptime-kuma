ba5ea22a4512ec98b224f9499d33455aea2bf41d
Louis Lam <louislam@users.noreply.github.com>
2026-02-13 10:07:30 +0800

    Fix Webpush

--- server/notification-providers/Webpush.js
+++ server/notification-providers/Webpush.js
@@ -18,24 +18,9 @@ class Webpush extends NotificationProvider {
 
             webpush.setVapidDetails("https://github.com/louislam/uptime-kuma", publicVapidKey, privateVapidKey);
 
-            if (heartbeatJSON === null && monitorJSON === null) {
-                // Test message
-                const data = JSON.stringify({
-                    title: "TEST",
-                    body: `Test Alert - ${msg}`,
-                });
-
-                await webpush.sendNotification(notification.subscription, data);
-
-                return okMsg;
-            }
-
             const data = JSON.stringify({
-                title: heartbeatJSON["status"] === UP ? "Monitor Up" : "Monitor DOWN",
-                body:
-                    heartbeatJSON["status"] === UP
-                        ? `❌ ${heartbeatJSON["name"]} is DOWN`
-                        : `✅ ${heartbeatJSON["name"]} is UP`,
+                title: "Uptime Kuma",
+                body: msg,
             });
 
             await webpush.sendNotification(notification.subscription, data);
