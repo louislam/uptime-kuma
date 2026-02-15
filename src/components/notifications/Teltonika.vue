a9bc1d9eb0e93e56b8b71ed647fabbb24d56e585
Tess Sluijter-Stek <tess@unixerius.nl>
2026-02-15 23:09:07 +0100

    Added recommendation to not use the admin account for sending SMS.

--- src/components/notifications/Teltonika.vue
+++ src/components/notifications/Teltonika.vue
@@ -39,6 +39,10 @@
             class="form-control"
             required
         />
+        <div class="form-text">
+            <i18n-t keypath="teltonikaUsernameHelptext" tag="div" class="form-text">
+            </i18n-t>
+        </div>
     </div>
     <div class="mb-3">
         <label for="teltonika-password" class="form-label">{{ $t("teltonikaPassword") }}</label>
