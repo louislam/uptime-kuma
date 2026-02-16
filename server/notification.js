593435c3cb0132c58d94118f5569824243c83ec9
Unixerius <97958845+unixerius@users.noreply.github.com>
2026-02-16 23:32:51 +0100

    feat: Notification provider Teltonika RUTxxx SMS gateway (#6952)
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>
    Co-authored-by: Frank Elsinga <frank@elsinga.de>

--- server/notification.js
+++ server/notification.js
@@ -60,6 +60,7 @@ const Stackfield = require("./notification-providers/stackfield");
 const Teams = require("./notification-providers/teams");
 const TechulusPush = require("./notification-providers/techulus-push");
 const Telegram = require("./notification-providers/telegram");
+const Teltonika = require("./notification-providers/teltonika");
 const Threema = require("./notification-providers/threema");
 const Twilio = require("./notification-providers/twilio");
 const Splunk = require("./notification-providers/splunk");
@@ -165,6 +166,7 @@ class Notification {
             new Teams(),
             new TechulusPush(),
             new Telegram(),
+            new Teltonika(),
             new Threema(),
             new Twilio(),
             new Splunk(),
