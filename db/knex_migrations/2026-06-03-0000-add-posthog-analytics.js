ae1d3ce715837711a289f0f12c0acfa9c519f85d
Nico Bleiler <nico@bleiler.dev>
2026-06-03 22:59:50 +0200

    feat(analytics): add PostHog to analytics type enum in status_page migration

---
+++ db/knex_migrations/2026-06-03-0000-add-posthog-analytics.js
@@ -0,0 +1,12 @@
+exports.up = function (knex) {
+    return knex.schema
+        .alterTable("status_page", function (table) {
+            table.enu("analytics_type", ["google", "umami", "plausible", "matomo", "posthog"]).defaultTo(null);
+        });
+};
+
+exports.down = function (knex) {
+    return knex.schema.alterTable("status_page", function (table) {
+            table.enu("analytics_type", ["google", "umami", "plausible", "matomo"]).defaultTo(null);
+    });
+};
