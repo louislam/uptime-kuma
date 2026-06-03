72211648e2df2587a925454ffd268a00d14c32e7
Nico Bleiler <nico@bleiler.dev>
2026-06-03 23:19:13 +0200

    fix(migration): add alter() to analytics_type column in PostHog migration

--- db/knex_migrations/2026-06-03-0000-add-posthog-analytics.js
+++ db/knex_migrations/2026-06-03-0000-add-posthog-analytics.js
@@ -1,12 +1,12 @@
 exports.up = function (knex) {
     return knex.schema
         .alterTable("status_page", function (table) {
-            table.enu("analytics_type", ["google", "umami", "plausible", "matomo", "posthog"]).defaultTo(null);
+            table.enu("analytics_type", ["google", "umami", "plausible", "matomo", "posthog"]).defaultTo(null).alter();
         });
 };
 
 exports.down = function (knex) {
     return knex.schema.alterTable("status_page", function (table) {
-            table.enu("analytics_type", ["google", "umami", "plausible", "matomo"]).defaultTo(null);
+            table.enu("analytics_type", ["google", "umami", "plausible", "matomo"]).defaultTo(null).alter();
     });
 };
