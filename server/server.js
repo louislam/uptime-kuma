fe2206e785dee9f23bba584c9b90e1adabcf215d
Louis Lam <louislam@users.noreply.github.com>
2026-06-25 11:14:24 +0800

    Fix binding issue

--- server/server.js
+++ server/server.js
@@ -71,7 +71,8 @@ if (process.env.UPTIME_KUMA_WS_ORIGIN_CHECK === "bypass") {
 
 if (isDev || process.env.UPTIME_KUMA_DEBUG_INSPECTOR === "1") {
     const inspector = require("inspector");
-    inspector.open();
+    let inspectorHost = "127.0.0.1";
+
     log.warn("server", "Node.js Inspector is enabled. You can connect to it via Chrome DevTools or VSCode.");
     log.warn("server", "Node.js Inspector is listening on:", inspector.url());
 
@@ -80,7 +81,10 @@ if (isDev || process.env.UPTIME_KUMA_DEBUG_INSPECTOR === "1") {
             "server",
             "You need to expose the port 9229:9229 in your docker command or docker compose, and ssh tunneling in order to connect to it."
         );
+        inspectorHost = "0.0.0.0";
     }
+
+    inspector.open(9229, inspectorHost);
 }
 
 const checkVersion = require("./check-version");
