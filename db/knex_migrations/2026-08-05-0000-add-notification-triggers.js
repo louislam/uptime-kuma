d27865796e842532e67083f6c5b4a75d419d8ba8
Marianne Le <berryanne.le@gmail.com>
2026-08-08 14:03:47 -0500

    fix: migration for adding triggers column should be safe to retry if malformed config

--- db/knex_migrations/2026-08-05-0000-add-notification-triggers.js
+++ db/knex_migrations/2026-08-05-0000-add-notification-triggers.js
@@ -1,27 +1,47 @@
 exports.up = async function (knex) {
-    await knex.schema.alterTable("notification", function (table) {
-        table.text("triggers").notNullable().defaultTo('["up","down","certificate","domain"]');
-    });
+    const notifications = await knex("notification").select("id", "config");
+    for (const { id, config } of notifications) {
+        try {
+            JSON.parse(config || "{}");
+        } catch (error) {
+            throw new Error(`Notification ${id} config is invalid JSON.`);
+        }
+    }
 
-    await knex("notification").whereNull("triggers").update({
-        triggers: '["up","down","certificate","domain"]',
-    });
-
-    const notifications = await knex("notification").select("*");
-    for (let n of notifications) {
+    for (const { id, config } of notifications) {
         await knex("notification")
-            .where("id", n.id)
+            .where("id", id)
             .update({
                 config: JSON.stringify({
-                    ...JSON.parse(n.config),
+                    ...JSON.parse(config || "{}"),
                     triggers: ["up", "down", "certificate", "domain"],
                 }),
             });
     }
+
+    await knex.schema.alterTable("notification", function (table) {
+        table.text("triggers").notNullable().defaultTo('["up","down","certificate","domain"]');
+    });
 };
 
 exports.down = function (knex) {
     return knex.schema.alterTable("notification", function (table) {
         table.dropColumn("triggers");
     });
+
+    const notifications = await knex("notification").select("id", "config");
+
+    for (const { id, config } of notifications) {
+        try {
+            const parsedConfig = JSON.parse(config || "{}");
+            delete parsedConfig.triggers;
+
+            await knex("notification")
+                .where("id", id)
+                .update({
+                    config: JSON.stringify(parsedConfig),
+                });
+        } catch (error) {
+            //
+        }
 };
