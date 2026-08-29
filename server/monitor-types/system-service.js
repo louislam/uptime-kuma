2dbb1fbf05ff1f8c22980ef2fd0988b561839c08
Tom Kent <tom@teeks99.com>
2026-08-29 07:39:20 -0500

    Upping to 30s as 10s wasn't enough

--- server/monitor-types/system-service.js
+++ server/monitor-types/system-service.js
@@ -85,8 +85,8 @@ class SystemServiceMonitorType extends MonitorType {
                 `(Get-Service -Name '${serviceName.replaceAll("'", "''")}').Status`,
             ];
 
-            // Using 10s timeout, powershell can take more than 5 sec to start on a Github runner
-            execFile(cmd, args, { timeout: 10000 }, (error, stdout, stderr) => {
+            // Using 30s timeout, powershell can take a bit to start on a Github runner
+            execFile(cmd, args, { timeout: 30000 }, (error, stdout, stderr) => {
                 let output = (stderr || stdout || "").toString().trim();
                 if (output.length > 200) {
                     output = output.substring(0, 200) + "...";
